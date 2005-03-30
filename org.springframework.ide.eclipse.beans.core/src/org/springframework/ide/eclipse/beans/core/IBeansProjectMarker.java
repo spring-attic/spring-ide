7198f0aab841626ee563208745ea79aecbe2f47c
Torsten Juergeleit <torsten@springide.org>
2005-03-30 23:43:47 +0000

    renamed one error code

--- org.springframework.ide.eclipse.beans.core/src/org/springframework/ide/eclipse/beans/core/IBeansProjectMarker.java
+++ org.springframework.ide.eclipse.beans.core/src/org/springframework/ide/eclipse/beans/core/IBeansProjectMarker.java
@@ -54,7 +54,7 @@ public interface IBeansProjectMarker {
 	 public static final int ERROR_CODE_BEAN_OVERRIDE = 2;
 	 public static final int ERROR_CODE_BEAN_WITHOUT_CLASS_OR_PARENT = 3;
 	 public static final int ERROR_CODE_CLASS_NOT_FOUND = 4;
-	 public static final int ERROR_CODE_UNDEFINED_PARENT_ROOT_BEAN = 5;
+	 public static final int ERROR_CODE_UNDEFINED_PARENT_BEAN = 5;
 	 public static final int ERROR_CODE_NO_CONSTRUCTOR = 6;
 	 public static final int ERROR_CODE_NO_SETTER = 7;
 	 public static final int ERROR_CODE_UNDEFINED_REFERENCE = 8;
