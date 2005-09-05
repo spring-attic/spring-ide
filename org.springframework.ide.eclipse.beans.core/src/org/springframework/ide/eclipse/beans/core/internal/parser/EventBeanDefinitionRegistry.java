5a785a42adc90925745d81e02c3aaaee17e5e6c5
Torsten Juergeleit <torsten@springide.org>
2005-09-05 22:53:16 +0000

    refactored from "org.springframework.ide.eclipse.core" to "org.springframework.ide.eclipse.beans.core"

--- org.springframework.ide.eclipse.beans.core/src/org/springframework/ide/eclipse/beans/core/internal/parser/EventBeanDefinitionRegistry.java
+++ org.springframework.ide.eclipse.beans.core/src/org/springframework/ide/eclipse/beans/core/internal/parser/EventBeanDefinitionRegistry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2004 the original author or authors.
+ * Copyright 2002-2005 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@ package org.springframework.ide.eclipse.beans.core.internal.parser;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.core.io.Resource;
-import org.springframework.ide.eclipse.core.beans.DefaultBeanDefinitionRegistry;
+import org.springframework.ide.eclipse.beans.core.DefaultBeanDefinitionRegistry;
 
 /**
  * Implementation of <code>BeanDefinitionRegistry</code> that reads bean
