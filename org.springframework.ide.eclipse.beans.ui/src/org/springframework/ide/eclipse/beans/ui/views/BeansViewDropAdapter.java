d09d17b1b24f462ba8efa1d613ecc1b1543742a0
Torsten Juergeleit <torsten@springide.org>
2006-01-03 22:32:39 +0000

    applied patch provided by Pierre-Antoine; fixes #205

--- org.springframework.ide.eclipse.beans.ui/src/org/springframework/ide/eclipse/beans/ui/views/BeansViewDropAdapter.java
+++ org.springframework.ide.eclipse.beans.ui/src/org/springframework/ide/eclipse/beans/ui/views/BeansViewDropAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -288,6 +288,7 @@ public class BeansViewDropAdapter extends ViewerDropAdapter {
 								result |= BeansViewDropAdapter.XML_FILE_SOURCE;
 							}
 						}
+						break;
 					default:
 						result |= UNKNOWN_SOURCE;
 						break;
