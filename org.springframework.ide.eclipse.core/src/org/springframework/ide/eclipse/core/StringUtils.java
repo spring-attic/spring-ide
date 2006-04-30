19f234b183e16e16947d9dfbb943a893374c7c4e
Torsten Juergeleit <torsten@springide.org>
2006-04-30 22:52:42 +0000

    made class final and updated year in copyright header

--- org.springframework.ide.eclipse.core/src/org/springframework/ide/eclipse/core/StringUtils.java
+++ org.springframework.ide.eclipse.core/src/org/springframework/ide/eclipse/core/StringUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2004 the original author or authors.
+ * Copyright 2002-2006 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ import java.util.Iterator;
 /**
  * Miscellaneous string utility methods.
  */
-public class StringUtils {
+public final class StringUtils {
 
 	public static final char SINGLE_QUOTE = '\'';
 	public static final char DOUBLE_QUOTE = '\"';
