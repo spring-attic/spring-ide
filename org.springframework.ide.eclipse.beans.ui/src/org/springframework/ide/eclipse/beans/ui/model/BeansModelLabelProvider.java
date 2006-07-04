4931df3ae890e16b2245601c4d89c0a0894e0997
Torsten Juergeleit <torsten@springide.org>
2006-07-04 23:34:46 +0000

    refactored creation of label for BeansModelElement into separate class

--- org.springframework.ide.eclipse.beans.ui/src/org/springframework/ide/eclipse/beans/ui/model/BeansModelLabelProvider.java
+++ org.springframework.ide.eclipse.beans.ui/src/org/springframework/ide/eclipse/beans/ui/model/BeansModelLabelProvider.java
@@ -12,99 +12,53 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ 
+ */
 
 package org.springframework.ide.eclipse.beans.ui.model;
 
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.config.BeanDefinitionHolder;
-import org.springframework.beans.factory.support.ChildBeanDefinition;
-import org.springframework.beans.factory.support.RootBeanDefinition;
-import org.springframework.ide.eclipse.beans.core.model.IBean;
-import org.springframework.ide.eclipse.beans.core.model.IBeanProperty;
+import org.eclipse.ui.model.WorkbenchLabelProvider;
 import org.springframework.ide.eclipse.core.SpringCoreUtils;
 import org.springframework.ide.eclipse.core.model.IModelElement;
-import org.springframework.util.StringUtils;
 
 /**
  * This class is an <code>ILabelProvider</code> which knows about the beans
  * core model's <code>IModelElement</code>s. If the given element is not of
  * type <code>IModelElement</code> the it tries to adapt it via
  * <code>IAdaptable</code>.
- *
+ * 
  * @see org.springframework.ide.eclipse.core.model.IModelElement
  * @see org.eclipse.core.runtime.IAdaptable
  * @author Torsten Juergeleit
  */
 public class BeansModelLabelProvider extends LabelProvider {
 
-	public Image getImage(Object element) {
+	WorkbenchLabelProvider wbLabelProvider;
+
+	public BeansModelLabelProvider() {
+		wbLabelProvider = new WorkbenchLabelProvider();
+	}
+
+    public void dispose() {
+    	wbLabelProvider.dispose();
+    	super.dispose();
+    }
+
+    public Image getImage(Object element) {
 		Object adaptedElement = SpringCoreUtils.adaptToModelElement(element);
 		if (adaptedElement instanceof IModelElement) {
 			return BeansModelImages.getImage((IModelElement) adaptedElement);
 		}
-		return null;
+		return wbLabelProvider.getImage(element);
 	}
 
 	public String getText(Object element) {
 		Object adaptedElement = SpringCoreUtils.adaptToModelElement(element);
 		if (adaptedElement instanceof IModelElement) {
-			return getText((IModelElement) adaptedElement);
+			return BeansModelElementLabels.getElementLabel(
+					(IModelElement) adaptedElement, 0);
 		}
-		return null;
-	}
-
-	public static String getText(IModelElement element) {
-		if (element instanceof IModelElement) {
-			StringBuffer label = new StringBuffer();
-			label.append(((IModelElement) element).getElementName());
-			if (element instanceof IBean) {
-				IBean bean = (IBean) element;
-				if (bean.getAliases() != null && bean.getAliases().length > 0) {
-					label.append(" '");
-					label.append(StringUtils.arrayToDelimitedString(bean.getAliases(), ", "));
-					label.append('\'');
-				}
-				if (bean.getClassName() != null) {
-					label.append(" [");
-					label.append(bean.getClassName());
-					label.append(']');
-				} else if (bean.getParentName() != null) {
-					label.append(" <");
-					label.append(bean.getParentName());
-					label.append('>');
-				}
-			} else if (element instanceof IBeanProperty) {
-				Object value = ((IBeanProperty) element).getValue();
-				if (value instanceof String) {
-					label.append(" \"");
-					label.append(value);
-					label.append('"');
-				} else if (value instanceof BeanDefinitionHolder) {
-					BeanDefinition beanDef = ((BeanDefinitionHolder) value)
-							.getBeanDefinition();
-					label.append(" {");
-					if (beanDef instanceof RootBeanDefinition) {
-						label.append('[');
-						label.append(((RootBeanDefinition)
-												   beanDef).getBeanClassName());
-						label.append(']');
-					} else {
-						label.append('<');
-						label.append(((ChildBeanDefinition)
-													  beanDef).getParentName());
-						label.append('>');
-					}
-					label.append('}');
-				} else {
-					label.append(' ');
-					label.append(value);
-				}
-			}
-			return label.toString();
-		}
-		return element.toString();
+		return wbLabelProvider.getText(element);
 	}
 }
