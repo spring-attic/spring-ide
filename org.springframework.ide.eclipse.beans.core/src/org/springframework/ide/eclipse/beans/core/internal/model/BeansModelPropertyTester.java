48082b1059bf985d15fc80758d449c6fa799c31f
Torsten Juergeleit <torsten@springide.org>
2006-09-01 21:34:49 +0000

    now reuses BeansCoreUtils.isBeansConfig() which does proper null checking; fixes #363

--- org.springframework.ide.eclipse.beans.core/src/org/springframework/ide/eclipse/beans/core/internal/model/BeansModelPropertyTester.java
+++ org.springframework.ide.eclipse.beans.core/src/org/springframework/ide/eclipse/beans/core/internal/model/BeansModelPropertyTester.java
@@ -12,38 +12,35 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ 
+ */
 
 package org.springframework.ide.eclipse.beans.core.internal.model;
 
 import org.eclipse.core.expressions.PropertyTester;
 import org.eclipse.core.resources.IFile;
-import org.springframework.ide.eclipse.beans.core.BeansCorePlugin;
-import org.springframework.ide.eclipse.beans.core.model.IBeansProject;
+import org.springframework.ide.eclipse.beans.core.BeansCoreUtils;
 
 /**
  * This <code>PropertyTester</code> is used to check properties of the
- * BeansCoreModel in in <code><test property="..."/></code> expressions.
- * Currently the following propeties are supported:
+ * BeansCoreModel in <code><test property="..."/></code> expressions.
+ * <p>
+ * Currently the following properties are supported:
  * <ul>
- * <li><strong>isBeansConfig</strong> checks if a given <code>IFile</code> is
- * 		a BeansConfig file</li>
+ * <li><strong>isBeansConfig</strong> checks if a given <code>IFile</code>
+ * is a BeansConfig file</li>
  * </ul>
+ * 
  * @author Torsten Juergeleit
  */
 public class BeansModelPropertyTester extends PropertyTester {
 
 	public boolean test(Object receiver, String property, Object[] args,
 			Object expectedValue) {
-		if (receiver instanceof IFile) {
-			IFile file = (IFile) receiver;
-			if ("isBeansConfig".equals(property)) {
-				IBeansProject project = BeansCorePlugin.getModel()
-						.getProject(file.getProject());
-				boolean isBeansConfig = project.hasConfig(file);
-				return expectedValue == null ? isBeansConfig :
+		if (receiver instanceof IFile && "isBeansConfig".equals(property)) {
+			boolean isBeansConfig = BeansCoreUtils
+					.isBeansConfig((IFile) receiver);
+			return expectedValue == null ? isBeansConfig :
 					isBeansConfig == ((Boolean) expectedValue).booleanValue();
-			}
 		}
 		return false;
 	}
