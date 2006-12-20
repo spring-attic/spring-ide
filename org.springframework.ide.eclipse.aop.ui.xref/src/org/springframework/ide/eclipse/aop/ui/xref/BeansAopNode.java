4dfa0828b50b8157aa3e64741cc575c15f341bcc
Christian Dupuis <cdupuis@vmware.com>
2006-12-20 15:31:15 +0000

    refactory support for AJDT's Cross Reference view to external plugin to avoid unstatisfied dependencies

---
+++ org.springframework.ide.eclipse.aop.ui.xref/src/org/springframework/ide/eclipse/aop/ui/xref/BeansAopNode.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.springframework.ide.eclipse.aop.ui.xref;
+
+import org.eclipse.contribution.xref.core.IXReferenceNode;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.ui.model.IWorkbenchAdapter;
+import org.springframework.ide.eclipse.aop.core.model.IAopReference;
+import org.springframework.ide.eclipse.aop.core.model.IAspectDefinition;
+import org.springframework.ide.eclipse.aop.ui.BeansAopUtils;
+import org.springframework.util.ObjectUtils;
+
+public class BeansAopNode implements IAdaptable, IXReferenceNode {
+
+    public enum TYPE {  
+        SOURCE, TARGET
+    };
+
+    private String label;
+
+    private IAopReference reference;
+
+    private TYPE type;
+
+    public BeansAopNode(TYPE type, IAopReference reference) {
+        this.reference = reference;
+        this.type = type;
+        computeLabel();
+    }
+
+    private void computeLabel() {
+        this.label = BeansAopUtils.getJavaElementLinkName(getJavaElement());
+        if (this.type.equals(TYPE.SOURCE)) {
+            this.label = this.label
+                    + BeansAopUtils.getElementDescription(reference);
+        }
+    }
+
+    /**
+     * @see IAdaptable#getAdapter(Class)
+     */
+    public Object getAdapter(Class adapter) {
+        if (adapter == IWorkbenchAdapter.class) {
+            return BeansAopNodeAdapter.getDefault();
+        }
+        return null;
+    }
+
+    public String getLabel() {
+        return this.label;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.contribution.xref.core.IXReferenceNode#getJavaElement()
+     */
+    public IJavaElement getJavaElement() {
+        if (this.type.equals(TYPE.SOURCE)) {
+            return this.reference.getSource();
+        }
+        else if (this.type.equals(TYPE.TARGET)) {
+            return this.reference.getTarget();
+        }
+        return null;
+    }
+
+    public IAspectDefinition getDefinition() {
+        return this.reference.getDefinition();
+    }
+
+    public IResource getResouce() {
+        return this.reference.getResource();
+    }
+
+    public boolean equals(Object obj) {
+        if (obj instanceof BeansAopNode) {
+            BeansAopNode other = (BeansAopNode) obj;
+            return getJavaElement().equals(other.getJavaElement())
+                    && getLabel().equals(other.getLabel())
+                    && getDefinition().getType() == other.getDefinition()
+                            .getType()
+                    && getDefinition().getAspectLineNumber() == other
+                            .getDefinition().getAspectLineNumber();
+        }
+        return false;
+    }
+    
+    public int hashCode() {
+        int hashCode = ObjectUtils.nullSafeHashCode(getJavaElement());
+        hashCode = 29 * hashCode + getDefinition().getType().hashCode();
+        hashCode = 24 * hashCode + ObjectUtils.nullSafeHashCode(getLabel());
+        hashCode = getDefinition().getAspectLineNumber() * 2 + hashCode;
+        return hashCode;
+    }
+}
---
+++ org.springframework.ide.eclipse.aop.ui.xref/src/org/springframework/ide/eclipse/aop/ui/xref/BeansAopNodeAdapter.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2002-2006 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.springframework.ide.eclipse.aop.ui.xref;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.model.IWorkbenchAdapter;
+
+class BeansAopNodeAdapter implements IWorkbenchAdapter {
+
+    protected BeansAopNodeAdapter() {
+    }
+
+    private static BeansAopNodeAdapter instance = null;
+
+    public static BeansAopNodeAdapter getDefault() {
+        if (instance == null) {
+            instance = new BeansAopNodeAdapter();
+        }
+        return instance;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.ui.model.IWorkbenchAdapter#getChildren(java.lang.Object)
+     */
+    public Object[] getChildren(Object o) {
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.ui.model.IWorkbenchAdapter#getImageDescriptor(java.lang.Object)
+     */
+    public ImageDescriptor getImageDescriptor(Object object) {
+        // For the moment we're leaving getting the icon to the decorator
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.ui.model.IWorkbenchAdapter#getLabel(java.lang.Object)
+     */
+    public String getLabel(Object o) {
+        if (o instanceof BeansAopNode) {
+            BeansAopNode node = (BeansAopNode) o;
+            return node.getLabel();
+        }
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.ui.model.IWorkbenchAdapter#getParent(java.lang.Object)
+     */
+    public Object getParent(Object o) {
+        return null;
+    }
+
+}
