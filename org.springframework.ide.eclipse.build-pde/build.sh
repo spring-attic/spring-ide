60675a9fb00981f90e03708b8af75deaf3b70300
Christian Dupuis <cdupuis@vmware.com>
2007-01-15 17:09:42 +0000

    some changes for build

--- org.springframework.ide.eclipse.build-pde/build.sh
+++ org.springframework.ide.eclipse.build-pde/build.sh
@@ -2,6 +2,6 @@
 
 # directory 
 JAVA_HOME=/usr/jdk1.5.0_07
-WORKSPACE=/home/springide/build
+WORKSPACE=`pwd`
 NAME=`date +%Y%m%d%H%M`
-$JAVA_HOME/bin/java -jar org.eclipse.releng.basebuilder/eclipse/startup.jar -application org.eclipse.ant.core.antRunner -buildfile $WORKSPACE/org.springframework.ide.eclipse.build-pde/org.eclipse.releng.basebuilder/eclipse/plugins/org.eclipse.pde.build_3.2.0.v20060505a/scripts/build.xml -Dbuilder=$WORKSPACE/org.springframework.ide.eclipse.build-pde/springide.builder -DforceContextQualifier=${NAME} 
+$JAVA_HOME/bin/java -jar org.eclipse.releng.basebuilder/eclipse/startup.jar -application org.eclipse.ant.core.antRunner -buildfile $WORKSPACE/org.eclipse.releng.basebuilder/eclipse/plugins/org.eclipse.pde.build_3.2.0.v20060505a/scripts/build.xml -Dbuilder=$WORKSPACE/springide.builder -DforceContextQualifier=${NAME} 
