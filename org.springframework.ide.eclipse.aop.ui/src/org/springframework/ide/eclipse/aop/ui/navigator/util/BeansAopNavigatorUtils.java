2162b3396433f09a63d3e841fef3f1cea590493c
Christian Dupuis <cdupuis@vmware.com>
2007-01-21 22:03:54 +0000

    added support for displaying references for <aop:*> elements in BeansCrossREferences view [fixes #439]

--- org.springframework.ide.eclipse.aop.ui/src/org/springframework/ide/eclipse/aop/ui/navigator/util/BeansAopNavigatorUtils.java
+++ org.springframework.ide.eclipse.aop.ui/src/org/springframework/ide/eclipse/aop/ui/navigator/util/BeansAopNavigatorUtils.java
@@ -51,236 +51,245 @@ import org.eclipse.swt.custom.StyledText;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPart;
 import org.springframework.ide.eclipse.aop.ui.Activator;
+import org.springframework.ide.eclipse.beans.ui.editor.BeansEditorUtils;
 import org.springframework.ide.eclipse.beans.ui.model.BeansModelLabelProvider;
+import org.springframework.util.StringUtils;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.w3c.dom.Text;
 
 @SuppressWarnings("restriction")
 public class BeansAopNavigatorUtils {
 
-    public static ILabelProvider JAVA_LABEL_PROVIDER = new DecoratingLabelProvider(
-            new JavaElementLabelProvider(JavaElementLabelProvider.SHOW_DEFAULT
-                    | JavaElementLabelProvider.SHOW_SMALL_ICONS), Activator
-                    .getDefault().getWorkbench().getDecoratorManager()
-                    .getLabelDecorator());
+	public static ILabelProvider JAVA_LABEL_PROVIDER = new DecoratingLabelProvider(
+			new JavaElementLabelProvider(JavaElementLabelProvider.SHOW_DEFAULT
+					| JavaElementLabelProvider.SHOW_SMALL_ICONS), Activator.getDefault()
+					.getWorkbench().getDecoratorManager().getLabelDecorator());
 
-    public static BeansModelLabelProvider BEAN_LABEL_PROVIDER = new BeansModelLabelProvider();
+	public static BeansModelLabelProvider BEAN_LABEL_PROVIDER = new BeansModelLabelProvider();
 
-    public static Object getSelectedElement(IWorkbenchPart part,
-            ISelection selection) {
-        Object selectedElement = getSelectedJavaElement(part, selection);
-        
+	public static Object getSelectedElement(IWorkbenchPart part, ISelection selection) {
+		Object selectedElement = getSelectedJavaElement(part, selection);
 
-        if (selectedElement == null) {
-            selectedElement = getSelectedXmlElement(selection);
-            
-        }
+		if (selectedElement == null) {
+			selectedElement = getSelectedXmlElement(selection);
 
-        return selectedElement;
-    }
+		}
 
-    private static Object getSelectedXmlElement(ISelection selection) {
-        Object selectedElement = null;
-        if (selection instanceof IStructuredSelection) {
-            IStructuredSelection structSelection = (IStructuredSelection) selection;
-            Object obj = structSelection.getFirstElement();
-            if (obj instanceof Element) {
-                selectedElement = obj;
-            }
-            else if (obj instanceof Text) {
-                Node parent = ((Text) obj).getParentNode();
-                if (parent instanceof Element) {
-                    selectedElement = parent;
-                }
-            }
-        }
-        return selectedElement;
-    }
+		return selectedElement;
+	}
 
-    public static IJavaElement getSelectedJavaElement(IWorkbenchPart part,
-            ISelection selection) {
-        if (selection instanceof IStructuredSelection) {
-            IStructuredSelection structuredSelection = (IStructuredSelection) selection;
-            Object first = structuredSelection.getFirstElement();
-            if (first instanceof IJavaElement) {
-                if (!(first instanceof IJavaProject)) {
-                    return (IJavaElement) first;
-                }
-            }
-        }
-        else if (part instanceof IEditorPart
-                && selection instanceof ITextSelection) {
-            if (part instanceof JavaEditor) {
-                JavaEditor je = (JavaEditor) part;
-                ISourceReference sourceRef = computeHighlightRangeSourceReference(je);
-                IJavaElement javaElement = (IJavaElement) sourceRef;
-                return javaElement;
-            }
-        }
-        return null;
-    }
+	private static Object getSelectedXmlElement(ISelection selection) {
+		Object selectedElement = null;
+		if (selection instanceof IStructuredSelection) {
+			IStructuredSelection structSelection = (IStructuredSelection) selection;
+			Object obj = structSelection.getFirstElement();
+			if (obj instanceof Element) {
+				Element elem = (Element) obj;
+				selectedElement = obj;
 
-    /**
-     * Computes and returns the source reference.
-     * 
-     * This is taken from the computeHighlightRangeSourceReference() method in
-     * the JavaEditor class which is used to populate the outline view
-     * 
-     * @return the computed source reference
-     */
-    public static ISourceReference computeHighlightRangeSourceReference(
-            JavaEditor editor) {
-        ISourceViewer sourceViewer = editor.getViewer();
-        if (sourceViewer == null)
-            return null;
+				if ("http://www.springframework.org/schema/aop".equals(elem.getNamespaceURI())) {
+					if ("aspect".equals(elem.getLocalName())) {
+						selectedElement = locateAspectReference(elem);
+					}
+					else if (!"config".equals(elem.getLocalName())) {
+						selectedElement = locateAspectReference(elem.getParentNode());
+					}
+				}
 
-        StyledText styledText = sourceViewer.getTextWidget();
-        if (styledText == null)
-            return null;
+			} else if (obj instanceof Text) {
+				Node parent = ((Text) obj).getParentNode();
+				if (parent instanceof Element) {
+					selectedElement = parent;
+				}
+			}
+		}
+		return selectedElement;
+	}
 
-        int caret = 0;
-        if (sourceViewer instanceof ITextViewerExtension5) {
-            ITextViewerExtension5 extension = (ITextViewerExtension5) sourceViewer;
-            caret = extension.widgetOffset2ModelOffset(styledText
-                    .getCaretOffset());
-        }
-        else {
-            int offset = sourceViewer.getVisibleRegion().getOffset();
-            caret = offset + styledText.getCaretOffset();
-        }
+	private static Object locateAspectReference(Node elem) {
+		Object selectedElement = null;
+		String aspectId = BeansEditorUtils.getAttribute(elem, "ref");
+		if (StringUtils.hasText(aspectId)) {
+			NodeList beans = elem.getOwnerDocument().getElementsByTagName("bean");
+			if (beans != null && beans.getLength() > 0) {
+				for (int i = 0; i < beans.getLength(); i++) {
+					Node node =  beans.item(i);
+					if (aspectId.equals(BeansEditorUtils.getAttribute(node, "id"))) {
+						selectedElement = node;
+						break;
+					}
+				}
+			}
+		}
+		return selectedElement;
+	}
 
-        IJavaElement element = getElementAt(editor, caret, false);
+	public static IJavaElement getSelectedJavaElement(IWorkbenchPart part, ISelection selection) {
+		if (selection instanceof IStructuredSelection) {
+			IStructuredSelection structuredSelection = (IStructuredSelection) selection;
+			Object first = structuredSelection.getFirstElement();
+			if (first instanceof IJavaElement) {
+				if (!(first instanceof IJavaProject)) {
+					return (IJavaElement) first;
+				}
+			}
+		} else if (part instanceof IEditorPart && selection instanceof ITextSelection) {
+			if (part instanceof JavaEditor) {
+				JavaEditor je = (JavaEditor) part;
+				ISourceReference sourceRef = computeHighlightRangeSourceReference(je);
+				IJavaElement javaElement = (IJavaElement) sourceRef;
+				return javaElement;
+			}
+		}
+		return null;
+	}
 
-        if (!(element instanceof ISourceReference))
-            return null;
+	/**
+	 * Computes and returns the source reference.
+	 * 
+	 * This is taken from the computeHighlightRangeSourceReference() method in the JavaEditor class
+	 * which is used to populate the outline view
+	 * 
+	 * @return the computed source reference
+	 */
+	public static ISourceReference computeHighlightRangeSourceReference(JavaEditor editor) {
+		ISourceViewer sourceViewer = editor.getViewer();
+		if (sourceViewer == null)
+			return null;
 
-        if (element.getElementType() == IJavaElement.IMPORT_DECLARATION) {
+		StyledText styledText = sourceViewer.getTextWidget();
+		if (styledText == null)
+			return null;
 
-            IImportDeclaration declaration = (IImportDeclaration) element;
-            IImportContainer container = (IImportContainer) declaration
-                    .getParent();
-            ISourceRange srcRange = null;
-            try {
-                srcRange = container.getSourceRange();
-            }
-            catch (JavaModelException e) {
-            }
+		int caret = 0;
+		if (sourceViewer instanceof ITextViewerExtension5) {
+			ITextViewerExtension5 extension = (ITextViewerExtension5) sourceViewer;
+			caret = extension.widgetOffset2ModelOffset(styledText.getCaretOffset());
+		} else {
+			int offset = sourceViewer.getVisibleRegion().getOffset();
+			caret = offset + styledText.getCaretOffset();
+		}
 
-            if (srcRange != null && srcRange.getOffset() == caret)
-                return container;
-        }
+		IJavaElement element = getElementAt(editor, caret, false);
 
-        return (ISourceReference) element;
-    }
+		if (!(element instanceof ISourceReference))
+			return null;
 
-    private static Map<Object, Object> workingCopyManagersForEditors = new HashMap<Object, Object>();
+		if (element.getElementType() == IJavaElement.IMPORT_DECLARATION) {
 
-    @SuppressWarnings("unused")
-    private static boolean selectedOutsideJavaElement = false;
+			IImportDeclaration declaration = (IImportDeclaration) element;
+			IImportContainer container = (IImportContainer) declaration.getParent();
+			ISourceRange srcRange = null;
+			try {
+				srcRange = container.getSourceRange();
+			} catch (JavaModelException e) {
+			}
 
-    /**
-     * Returns the most narrow java element including the given offset.
-     * 
-     * This is taken from the getElementAt(int offset, boolean reconcile) method
-     * in the CompilationUnitEditor class.
-     */
-    private static IJavaElement getElementAt(JavaEditor editor, int offset,
-            boolean reconcile) {
-        IWorkingCopyManager manager;
-        if (workingCopyManagersForEditors.get(editor) instanceof IWorkingCopyManager) {
-            manager = (IWorkingCopyManager) workingCopyManagersForEditors
-                    .get(editor);
-        }
-        else {
-            manager = JavaPlugin.getDefault().getWorkingCopyManager();
-        }
-        ICompilationUnit unit = manager.getWorkingCopy(editor.getEditorInput());
+			if (srcRange != null && srcRange.getOffset() == caret)
+				return container;
+		}
 
-        if (unit != null) {
-            try {
-                if (reconcile) {
-                    synchronized (unit) {
-                        unit.reconcile(ICompilationUnit.NO_AST, false, null,
-                                null);
-                    }
-                    IJavaElement elementAt = unit.getElementAt(offset);
-                    if (elementAt != null) {
-                        return elementAt;
-                    }
-                    // this is if the selection in the editor
-                    // is outside the {} of the class or aspect
-                    IJavaElement[] children = unit.getChildren();
-                    for (int i = 0; i < children.length; i++) {
-                        if (children[i] instanceof SourceType) {
-                            return children[i];
-                        }
-                    }
-                }
-                else if (unit.isConsistent()) {
-                    // Bug 96313 - if there is no IJavaElement for the
-                    // given offset, then check whether there are any
-                    // children for this CU. There are if you've selected
-                    // somewhere in the file and there aren't if there are
-                    // compilation errors. Therefore, return one of these
-                    // children and calculate the xrefs as though the user
-                    // wants to display the xrefs for the entire file
-                    IJavaElement elementAt = unit.getElementAt(offset);
-                    if (elementAt != null) {
-                        // a javaElement has been selected, therefore
-                        // no need to go any further
-                        return elementAt;
-                    }
-                    IResource res = unit.getCorrespondingResource();
-                    if (res instanceof IFile) {
-                        IFile file = (IFile) res;
-                        IProject containingProject = file.getProject();
-                        IMarker[] javaModelMarkers = containingProject
-                                .findMarkers(
-                                        IJavaModelMarker.JAVA_MODEL_PROBLEM_MARKER,
-                                        false, IResource.DEPTH_INFINITE);
-                        for (int i = 0; i < javaModelMarkers.length; i++) {
-                            IMarker marker = javaModelMarkers[i];
-                            if (marker.getResource().equals(file)) {
-                                // there is an error in the file, therefore
-                                // we don't want to return any xrefs
-                                return null;
-                            }
-                        }
-                    }
-                    // the selection was outside an IJavaElement, however, there
-                    // are children for this compilation unit so we think you've
-                    // selected outside of a java element.
-                    if (elementAt == null && unit.getChildren().length != 0) {
-                        selectedOutsideJavaElement = true;
-                        return unit.getChildren()[0];
-                    }
-                }
+		return (ISourceReference) element;
+	}
 
-            }
-            catch (JavaModelException x) {
-                if (!x.isDoesNotExist())
-                    JavaPlugin.log(x.getStatus());
-                // nothing found, be tolerant and go on
-            }
-            catch (CoreException e) {
-            }
-        }
+	private static Map<Object, Object> workingCopyManagersForEditors = new HashMap<Object, Object>();
 
-        return null;
-    }
+	@SuppressWarnings("unused")
+	private static boolean selectedOutsideJavaElement = false;
 
-    public static int getLineNumber(IMember member) {
-        try {
-            ICompilationUnit compUnit = member.getCompilationUnit();
-            Document document = new Document(compUnit.getBuffer().getContents());
-            return document
-                    .getLineOfOffset(member.getSourceRange().getOffset());
-        }
-        catch (JavaModelException e) {
-        }
-        catch (BadLocationException e) {
-        }
-        return -1;
-    }
+	/**
+	 * Returns the most narrow java element including the given offset.
+	 * 
+	 * This is taken from the getElementAt(int offset, boolean reconcile) method in the
+	 * CompilationUnitEditor class.
+	 */
+	private static IJavaElement getElementAt(JavaEditor editor, int offset, boolean reconcile) {
+		IWorkingCopyManager manager;
+		if (workingCopyManagersForEditors.get(editor) instanceof IWorkingCopyManager) {
+			manager = (IWorkingCopyManager) workingCopyManagersForEditors.get(editor);
+		} else {
+			manager = JavaPlugin.getDefault().getWorkingCopyManager();
+		}
+		ICompilationUnit unit = manager.getWorkingCopy(editor.getEditorInput());
+
+		if (unit != null) {
+			try {
+				if (reconcile) {
+					synchronized (unit) {
+						unit.reconcile(ICompilationUnit.NO_AST, false, null, null);
+					}
+					IJavaElement elementAt = unit.getElementAt(offset);
+					if (elementAt != null) {
+						return elementAt;
+					}
+					// this is if the selection in the editor
+					// is outside the {} of the class or aspect
+					IJavaElement[] children = unit.getChildren();
+					for (int i = 0; i < children.length; i++) {
+						if (children[i] instanceof SourceType) {
+							return children[i];
+						}
+					}
+				} else if (unit.isConsistent()) {
+					// Bug 96313 - if there is no IJavaElement for the
+					// given offset, then check whether there are any
+					// children for this CU. There are if you've selected
+					// somewhere in the file and there aren't if there are
+					// compilation errors. Therefore, return one of these
+					// children and calculate the xrefs as though the user
+					// wants to display the xrefs for the entire file
+					IJavaElement elementAt = unit.getElementAt(offset);
+					if (elementAt != null) {
+						// a javaElement has been selected, therefore
+						// no need to go any further
+						return elementAt;
+					}
+					IResource res = unit.getCorrespondingResource();
+					if (res instanceof IFile) {
+						IFile file = (IFile) res;
+						IProject containingProject = file.getProject();
+						IMarker[] javaModelMarkers = containingProject.findMarkers(
+								IJavaModelMarker.JAVA_MODEL_PROBLEM_MARKER, false,
+								IResource.DEPTH_INFINITE);
+						for (int i = 0; i < javaModelMarkers.length; i++) {
+							IMarker marker = javaModelMarkers[i];
+							if (marker.getResource().equals(file)) {
+								// there is an error in the file, therefore
+								// we don't want to return any xrefs
+								return null;
+							}
+						}
+					}
+					// the selection was outside an IJavaElement, however, there
+					// are children for this compilation unit so we think you've
+					// selected outside of a java element.
+					if (elementAt == null && unit.getChildren().length != 0) {
+						selectedOutsideJavaElement = true;
+						return unit.getChildren()[0];
+					}
+				}
+
+			} catch (JavaModelException x) {
+				if (!x.isDoesNotExist())
+					JavaPlugin.log(x.getStatus());
+				// nothing found, be tolerant and go on
+			} catch (CoreException e) {
+			}
+		}
+
+		return null;
+	}
+
+	public static int getLineNumber(IMember member) {
+		try {
+			ICompilationUnit compUnit = member.getCompilationUnit();
+			Document document = new Document(compUnit.getBuffer().getContents());
+			return document.getLineOfOffset(member.getSourceRange().getOffset());
+		} catch (JavaModelException e) {
+		} catch (BadLocationException e) {
+		}
+		return -1;
+	}
 }
