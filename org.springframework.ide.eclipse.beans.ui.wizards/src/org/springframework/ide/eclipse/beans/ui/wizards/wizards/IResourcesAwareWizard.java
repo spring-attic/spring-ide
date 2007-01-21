06c9767589d510c31107f77be11d43c4cf4cd2b3
Christian Dupuis <cdupuis@vmware.com>
2007-01-21 19:56:36 +0000

    moved wizards plugin to branches/wizards to avoid tagging until the wizards plugins are completed

--- org.springframework.ide.eclipse.beans.ui.wizards/src/org/springframework/ide/eclipse/beans/ui/wizards/wizards/IResourcesAwareWizard.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.ide.eclipse.beans.ui.wizards.wizards;
-
-import org.eclipse.jdt.core.IType;
-import org.springframework.ide.eclipse.beans.core.model.IBeansConfig;
-import org.springframework.ide.eclipse.beans.core.model.IBeansProject;
-
-public interface IResourcesAwareWizard {
-	public void init(IBeansProject project, IBeansConfig config, IType type);
-}
