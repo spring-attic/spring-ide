51032b949563d7aa28a5fea3c869df076aeb582f
Torsten Juergeleit <torsten@springide.org>
2007-02-17 22:41:12 +0000

    now getAdapter() finally delegates to the wrapped IStorage's getAdapter()

--- org.springframework.ide.eclipse.core/src/org/springframework/ide/eclipse/core/io/StorageResource.java
+++ org.springframework.ide.eclipse.core/src/org/springframework/ide/eclipse/core/io/StorageResource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2006 the original author or authors.
+ * Copyright 2002-2007 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -66,7 +66,7 @@ public class StorageResource extends AbstractResource implements IAdaptable {
 		if (adapter.equals(IStorage.class)) {
 			return storage;
 		}
-		return null;
+		return storage.getAdapter(adapter);
 	}
 
 	public boolean equals(Object other) {
