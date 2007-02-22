37fa82c8ad26af007a4fff6109982d77919cab98
Christian Dupuis <cdupuis@vmware.com>
2007-02-22 18:36:21 +0000

    added contribution to beans.core.namespaces and beans.ui.namespaces extension point

---
+++ org.springframework.ide.eclipse.webflow.ui.editor/src/org/springframework/ide/eclipse/beans/ui/namespaces/WebflowLabelProvider.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2002-2007 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.ide.eclipse.beans.ui.namespaces;
+
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.graphics.Image;
+import org.springframework.ide.eclipse.beans.ui.model.BeansModelLabelProvider;
+import org.springframework.ide.eclipse.webflow.ui.editor.namespaces.webflow.WebflowUIImages;
+
+public class WebflowLabelProvider extends LabelProvider {
+
+	public Image getImage(Object element) {
+		return WebflowUIImages.getImage(WebflowUIImages.IMG_OBJS_WEBFLOW);
+	}
+
+	public String getText(Object element) {
+		return BeansModelLabelProvider.DEFAULT_NAMESPACE_LABEL_PROVIDER
+				.getText(element);
+	}
+}
