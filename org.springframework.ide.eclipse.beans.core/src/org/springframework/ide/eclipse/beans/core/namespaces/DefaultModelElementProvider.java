7273fe828d3a9b120e393d4d7d58d176678de6b4
Torsten Juergeleit <torsten@springide.org>
2007-02-23 00:20:32 +0000

    added javadoc

--- org.springframework.ide.eclipse.beans.core/src/org/springframework/ide/eclipse/beans/core/namespaces/DefaultModelElementProvider.java
+++ org.springframework.ide.eclipse.beans.core/src/org/springframework/ide/eclipse/beans/core/namespaces/DefaultModelElementProvider.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2002-2007 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.ide.eclipse.beans.core.namespaces;
 
 import org.springframework.beans.factory.config.BeanDefinition;
@@ -14,6 +30,14 @@ import org.springframework.ide.eclipse.beans.core.model.IBeansComponent;
 import org.springframework.ide.eclipse.beans.core.model.IBeansConfig;
 import org.springframework.ide.eclipse.core.model.ISourceModelElement;
 
+/**
+ * This class is an {@link IModelElementProvider} which converts a given
+ * {@link CompositeComponentDefinition} into a single {@link IBean} or an
+ * {@link IBeansComponent} containing {@link IBean}(s) or
+ * {@link IBeansComponent}(s).
+ * 
+ * @author Torsten Juergeleit
+ */
 public class DefaultModelElementProvider implements IModelElementProvider {
 
 	public ISourceModelElement getElement(IBeansConfig config,
