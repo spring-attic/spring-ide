cdb86a794f72f9855e4dff60ac956e3fe00fbe2a
Christian Dupuis <cdupuis@vmware.com>
2007-03-17 19:34:59 +0000

    pohlishe

--- org.springframework.ide.eclipse.webflow.ui.graph/src/org/springframework/ide/eclipse/webflow/ui/graph/parts/FlowPart.java
+++ org.springframework.ide.eclipse.webflow.ui.graph/src/org/springframework/ide/eclipse/webflow/ui/graph/parts/FlowPart.java
@@ -37,162 +37,109 @@ import org.springframework.ide.eclipse.webflow.ui.graph.policies.FlowEditPolicy;
 import org.springframework.ide.eclipse.webflow.ui.graph.policies.FlowStateLayoutEditPolicy;
 import org.springframework.ide.eclipse.webflow.ui.graph.policies.StateContainerEditPolicy;
 
-/**
- * 
- * 
- * @author hudsonr Created on Jul 16, 2003
- */
 public class FlowPart extends ChildrenStatePart implements
-        PropertyChangeListener {
+		PropertyChangeListener {
 
-    /**
-     * 
-     */
-    String CONNECTION_LAYER = "Connection Layer";
+	private static final String CONNECTION_LAYER = "Connection Layer";
 
-    /**
-     * 
-     */
-    AnimatedFanConnectionRouter fanRouter = null;
+	private AnimatedFanConnectionRouter fanRouter = null;
 
-    /**
-     * 
-     */
-    ShortestPathConnectionRouter router = null;
+	private ShortestPathConnectionRouter router = null;
 
-    /**
-     * 
-     */
-    CommandStackListener stackListener = new CommandStackListener() {
+	private CommandStackListener stackListener = new CommandStackListener() {
 
-        public void commandStackChanged(EventObject event) {
-            if (!GraphAnimation.captureLayout(getFigure()))
-                return;
-            while (GraphAnimation.step())
-                getFigure().getUpdateManager().performUpdate();
-            GraphAnimation.end();
-        }
-    };
+		public void commandStackChanged(EventObject event) {
+			if (!GraphAnimation.captureLayout(getFigure()))
+				return;
+			while (GraphAnimation.step())
+				getFigure().getUpdateManager().performUpdate();
+			GraphAnimation.end();
+		}
+	};
 
-    /**
-     * 
-     * 
-     * @see org.springframework.ide.eclipse.webflow.ui.graph.parts.AbstractStatePart#activate()
-     */
-    public void activate() {
-        super.activate();
-        getViewer().getEditDomain().getCommandStack().addCommandStackListener(
-                stackListener);
-    }
+	public void activate() {
+		super.activate();
+		getViewer().getEditDomain().getCommandStack().addCommandStackListener(
+				stackListener);
+	}
 
-    /* (non-Javadoc)
-     * @see org.springframework.ide.eclipse.webflow.ui.graph.parts.ChildrenStatePart#applyOwnResults(org.eclipse.draw2d.graph.CompoundDirectedGraph, java.util.Map)
-     */
-    protected void applyOwnResults(CompoundDirectedGraph graph, Map map) {
-    }
+	protected void applyOwnResults(CompoundDirectedGraph graph, Map map) {
+	}
 
-    /**
-     * 
-     * 
-     * @see org.springframework.ide.eclipse.webflow.ui.graph.parts.AbstractStatePart#createEditPolicies()
-     */
-    protected void createEditPolicies() {
-        installEditPolicy(EditPolicy.NODE_ROLE, null);
-        installEditPolicy(EditPolicy.GRAPHICAL_NODE_ROLE, null);
-        installEditPolicy(EditPolicy.SELECTION_FEEDBACK_ROLE, null);
-        installEditPolicy(EditPolicy.COMPONENT_ROLE,
-                new FlowEditPolicy());
-        installEditPolicy(EditPolicy.LAYOUT_ROLE,
-                new FlowStateLayoutEditPolicy());
-        installEditPolicy(EditPolicy.CONTAINER_ROLE,
-                new StateContainerEditPolicy());
-    }
+	protected void createEditPolicies() {
+		installEditPolicy(EditPolicy.NODE_ROLE, null);
+		installEditPolicy(EditPolicy.GRAPHICAL_NODE_ROLE, null);
+		installEditPolicy(EditPolicy.SELECTION_FEEDBACK_ROLE, null);
+		installEditPolicy(EditPolicy.COMPONENT_ROLE, new FlowEditPolicy());
+		installEditPolicy(EditPolicy.LAYOUT_ROLE,
+				new FlowStateLayoutEditPolicy());
+		installEditPolicy(EditPolicy.CONTAINER_ROLE,
+				new StateContainerEditPolicy());
+	}
 
-    /* (non-Javadoc)
-     * @see org.springframework.ide.eclipse.webflow.ui.graph.parts.ChildrenStatePart#createFigure()
-     */
-    protected IFigure createFigure() {
-        Figure f = new Figure() {
+	protected IFigure createFigure() {
+		Figure f = new Figure() {
 
-            public void setBounds(Rectangle rect) {
-                int x = bounds.x, y = bounds.y;
+			public void setBounds(Rectangle rect) {
+				int x = bounds.x, y = bounds.y;
 
-                boolean resize = (rect.width != bounds.width)
-                        || (rect.height != bounds.height), translate = (rect.x != x)
-                        || (rect.y != y);
+				boolean resize = (rect.width != bounds.width)
+						|| (rect.height != bounds.height), translate = (rect.x != x)
+						|| (rect.y != y);
 
-                if (isVisible() && (resize || translate))
-                    erase();
-                if (translate) {
-                    int dx = rect.x - x;
-                    int dy = rect.y - y;
-                    primTranslate(dx, dy);
-                }
-                bounds.width = rect.width;
-                bounds.height = rect.height;
-                if (resize || translate) {
-                    fireFigureMoved();
-                    repaint();
-                }
-            }
-        };
-        f.setLayoutManager(new GraphLayoutManager(this));
-        return f;
-    }
+				if (isVisible() && (resize || translate))
+					erase();
+				if (translate) {
+					int dx = rect.x - x;
+					int dy = rect.y - y;
+					primTranslate(dx, dy);
+				}
+				bounds.width = rect.width;
+				bounds.height = rect.height;
+				if (resize || translate) {
+					fireFigureMoved();
+					repaint();
+				}
+			}
+		};
 
-    /**
-     * 
-     * 
-     * @see org.springframework.ide.eclipse.webflow.ui.graph.parts.AbstractStatePart#deactivate()
-     */
-    public void deactivate() {
-        getViewer().getEditDomain().getCommandStack()
-                .removeCommandStackListener(stackListener);
-        super.deactivate();
-    }
+		f.setLayoutManager(new GraphLayoutManager(this));
+		return f;
+	}
 
-    /* (non-Javadoc)
-     * @see org.springframework.ide.eclipse.webflow.ui.graph.parts.AbstractStatePart#getModelChildren()
-     */
-    protected List getModelChildren() {
-        if (((IWebflowState) getState()).getStates() != null) {
-            List<IState> states = new ArrayList<IState>();
-            states.addAll(((IWebflowState) getState()).getStates());
-            states.addAll(((IWebflowState) getState()).getInlineFlowStates());
-            return states;
-        }
-        else {
-            return Collections.EMPTY_LIST;
-        }
-    }
+	public void deactivate() {
+		getViewer().getEditDomain().getCommandStack()
+				.removeCommandStackListener(stackListener);
+		super.deactivate();
+	}
 
-    /**
-     * 
-     * 
-     * @return 
-     * 
-     * @see org.eclipse.gef.editparts.AbstractEditPart#isSelectable()
-     */
-    public boolean isSelectable() {
-        return true;
-    }
+	protected List getModelChildren() {
+		if (((IWebflowState) getState()).getStates() != null) {
+			List<IState> states = new ArrayList<IState>();
+			states.addAll(((IWebflowState) getState()).getStates());
+			states.addAll(((IWebflowState) getState()).getInlineFlowStates());
+			return states;
+		}
+		else {
+			return Collections.EMPTY_LIST;
+		}
+	}
 
-    /**
-     * 
-     * 
-     * @see org.springframework.ide.eclipse.webflow.ui.graph.parts.ChildrenStatePart#refreshVisuals()
-     */
-    protected void refreshVisuals() {
+	public boolean isSelectable() {
+		return true;
+	}
 
-        ConnectionLayer cLayer = (ConnectionLayer) getLayer(CONNECTION_LAYER);
-        cLayer.setAntialias(SWT.ON);
-        if (fanRouter == null && router == null) {
-            fanRouter = new AnimatedFanConnectionRouter();
-            fanRouter.setSeparation(20);
-            router = new ShortestPathConnectionRouter(getFigure());
-        }
-        
-        fanRouter.setNextRouter(router);
-        cLayer.setConnectionRouter(fanRouter);
-    }
+	protected void refreshVisuals() {
+
+		ConnectionLayer cLayer = (ConnectionLayer) getLayer(CONNECTION_LAYER);
+		cLayer.setAntialias(SWT.ON);
+		if (fanRouter == null && router == null) {
+			fanRouter = new AnimatedFanConnectionRouter();
+			fanRouter.setSeparation(20);
+			router = new ShortestPathConnectionRouter(getFigure());
+		}
+
+		fanRouter.setNextRouter(router);
+		cLayer.setConnectionRouter(fanRouter);
+	}
 }
