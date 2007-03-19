8258d6544811e26772dccd31a5ac0df7bc5a4f11
Christian Dupuis <cdupuis@vmware.com>
2007-03-19 14:48:47 +0000

    added support for setter and field reference search [fixes #544]

--- org.springframework.ide.eclipse.beans.ui.search/src/org/springframework/ide/eclipse/beans/ui/search/jdt/BeansJavaSearchParticipant.java
+++ org.springframework.ide.eclipse.beans.ui.search/src/org/springframework/ide/eclipse/beans/ui/search/jdt/BeansJavaSearchParticipant.java
@@ -12,13 +12,19 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ 
+ */
 
 package org.springframework.ide.eclipse.beans.ui.search.jdt;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.IField;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.ui.search.ElementQuerySpecification;
 import org.eclipse.jdt.ui.search.IMatchPresentation;
 import org.eclipse.jdt.ui.search.IQueryParticipant;
@@ -29,6 +35,7 @@ import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.search.ui.ISearchQuery;
 import org.eclipse.search.ui.text.Match;
 import org.eclipse.ui.PartInitException;
+import org.springframework.ide.eclipse.beans.core.internal.model.BeansModelUtils;
 import org.springframework.ide.eclipse.beans.core.model.IBean;
 import org.springframework.ide.eclipse.beans.ui.search.internal.BeansSearchResult;
 import org.springframework.ide.eclipse.beans.ui.search.internal.BeansSearchScope;
@@ -36,12 +43,12 @@ import org.springframework.ide.eclipse.beans.ui.search.internal.queries.BeanClas
 import org.springframework.ide.eclipse.beans.ui.search.internal.queries.BeanPropertyQuery;
 import org.springframework.ide.eclipse.core.model.IResourceModelElement;
 import org.springframework.ide.eclipse.ui.SpringUIUtils;
+import org.springframework.util.StringUtils;
 
 /**
  * {@link IQueryParticipant} implementation that hooks into JDT's Java reference
  * search and displays {@link IBean} that have the class or property name under
  * question
- * 
  * @author Christian Dupuis
  * @author Torsten Juergeleit
  * @since 2.0
@@ -50,8 +57,11 @@ public class BeansJavaSearchParticipant implements IQueryParticipant,
 		IMatchPresentation {
 
 	private static final int S_LIMIT_REF = 2;
+
 	private static final int S_LIMIT_ALL = 3;
+
 	private static final int S_FOR_TYPES = 0;
+
 	private static final int S_FOR_FIELDS = 1;
 
 	private int searchFor = -1;
@@ -73,23 +83,43 @@ public class BeansJavaSearchParticipant implements IQueryParticipant,
 		}
 
 		String search = null;
+		List<String> requiredTypeNames = new ArrayList<String>();
+
 		if (querySpecification instanceof ElementQuerySpecification) {
-			ElementQuerySpecification elementQuerySpecification =
-					(ElementQuerySpecification) querySpecification;
+			ElementQuerySpecification elementQuerySpecification = (ElementQuerySpecification) querySpecification;
 			if (elementQuerySpecification.getElement() instanceof IType) {
 				search = ((IType) elementQuerySpecification.getElement())
 						.getFullyQualifiedName();
+			}
+			else if (elementQuerySpecification.getElement() instanceof IField) {
+				IField field = ((IField) elementQuerySpecification.getElement());
+				search = field.getElementName();
+				getTypeHierachy(monitor, requiredTypeNames, field.getDeclaringType());
+			}
+			else if (elementQuerySpecification.getElement() instanceof IMethod) {
+				IMethod method = ((IMethod) elementQuerySpecification
+						.getElement());
+				search = method.getElementName();
+				// do property name magic 
+				if (search.startsWith("set")) {
+					search = StringUtils.uncapitalize(search.substring(3));
+				}
+				getTypeHierachy(monitor, requiredTypeNames, method.getDeclaringType());
 			}
 			else {
 				search = elementQuerySpecification.getElement()
 						.getElementName();
 			}
+			
 			int type = ((ElementQuerySpecification) querySpecification)
 					.getElement().getElementType();
 			if (type == IJavaElement.TYPE) {
 				searchFor = S_FOR_TYPES;
 			}
 			else if (type == IJavaElement.FIELD) {
+				searchFor = S_FOR_FIELDS;
+			}
+			else if (type == IJavaElement.METHOD) {
 				searchFor = S_FOR_FIELDS;
 			}
 		}
@@ -99,6 +129,7 @@ public class BeansJavaSearchParticipant implements IQueryParticipant,
 			search = ((PatternQuerySpecification) querySpecification)
 					.getPattern();
 		}
+
 		ISearchQuery query = null;
 		BeansSearchScope scope = BeansSearchScope.newSearchScope();
 		if (searchFor == S_FOR_TYPES) {
@@ -107,6 +138,7 @@ public class BeansJavaSearchParticipant implements IQueryParticipant,
 		else if (searchFor == S_FOR_FIELDS) {
 			query = new BeanPropertyQuery(scope, search, true, false);
 		}
+
 		if (query != null) {
 			query.run(monitor);
 
@@ -116,15 +148,41 @@ public class BeansJavaSearchParticipant implements IQueryParticipant,
 				Match[] matches = searchResult.getMatches(obj);
 				if (matches != null && matches.length > 0) {
 					for (Match match : matches) {
-						requestor.reportMatch(match);
+
+						if (searchFor == S_FOR_FIELDS) {
+							// check if the match fits to the selected class
+							IBean bean = (IBean) match.getElement();
+							String beanClass = BeansModelUtils.getBeanClass(
+									bean, null);
+							if (requiredTypeNames.contains(beanClass)) {
+								requestor.reportMatch(match);
+							}
+						}
+						else {
+							requestor.reportMatch(match);
+						}
 					}
 				}
 			}
 		}
 	}
 
+	private void getTypeHierachy(IProgressMonitor monitor,
+			List<String> requiredTypeNames, IType baseType) {
+		try {
+			IType[] types = baseType.newTypeHierarchy(monitor).getAllSubtypes(
+					baseType);
+			requiredTypeNames.add(baseType.getFullyQualifiedName());
+			for (IType type : types) {
+				requiredTypeNames.add(type.getFullyQualifiedName());
+			}
+		}
+		catch (JavaModelException e) {
+		}
+	}
+
 	public ILabelProvider createLabelProvider() {
-		
+
 		// This label provider will be disposed by the search page
 		return new BeansJavaSearchLabelProvider(true);
 	}
