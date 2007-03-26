01404d673ab98e650cd2ac25696b2fac3dedffcb
Christian Dupuis <cdupuis@vmware.com>
2007-03-26 09:44:55 +0000

    changed Spring IDE's OSS license to EPL v1.0 [fixes #550]

--- org.springframework.ide.eclipse.aop.ui/src/org/springframework/ide/eclipse/aop/ui/navigator/model/MethodReference.java
+++ org.springframework.ide.eclipse.aop.ui/src/org/springframework/ide/eclipse/aop/ui/navigator/model/MethodReference.java
@@ -1,7 +1,13 @@
-/*
- * Copyright 2004 DekaBank Deutsche Girozentrale. All rights reserved.
- */
-
+/*******************************************************************************
+ * Copyright (c) 2005, 2007 Spring IDE Developers
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Spring IDE Developers - initial API and implementation
+ *******************************************************************************/
 package org.springframework.ide.eclipse.aop.ui.navigator.model;
 
 import java.util.ArrayList;
--- org.springframework.ide.eclipse.test/src/org/springframework/ide/test/TestProject.java
+++ org.springframework.ide.eclipse.test/src/org/springframework/ide/test/TestProject.java
@@ -1,13 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2003 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
+ * Copyright (c) 2005, 2007 Spring IDE Developers
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *     Erich Gamma (erich_gamma@ch.ibm.com) and
- *         Kent Beck (kent@threeriversinstitute.org)
+ *     Spring IDE Developers - initial API and implementation
  *******************************************************************************/
 package org.springframework.ide.test;
 
