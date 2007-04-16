4b5ed4126037f055069ab9a7f691fd2c1497fd7f
Torsten Juergeleit <torsten@springide.org>
2007-04-16 21:34:02 +0000

    now uses IFile instead of IBeansConfig as parent node of the matching IBeans

--- org.springframework.ide.eclipse.beans.ui.search/src/org/springframework/ide/eclipse/beans/ui/search/internal/BeansSearchContentProvider.java
+++ org.springframework.ide.eclipse.beans.ui.search/src/org/springframework/ide/eclipse/beans/ui/search/internal/BeansSearchContentProvider.java
@@ -13,8 +13,10 @@ package org.springframework.ide.eclipse.beans.ui.search.internal;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.resources.IFile;
 import org.springframework.ide.eclipse.beans.core.BeansCorePlugin;
 import org.springframework.ide.eclipse.beans.core.internal.model.BeansModelUtils;
+import org.springframework.ide.eclipse.beans.core.model.IBean;
 import org.springframework.ide.eclipse.beans.core.model.IBeansConfig;
 import org.springframework.ide.eclipse.beans.ui.model.BeansModelContentProvider;
 import org.springframework.ide.eclipse.core.model.IModelElement;
@@ -51,29 +53,37 @@ public class BeansSearchContentProvider extends BeansModelContentProvider {
 	}
 
 	@Override
+    public boolean hasChildren(Object element) {
+		if (element instanceof IBean) {
+			return false;
+		}
+		return super.hasChildren(element);
+	}
+
+	@Override
 	public Object[] getChildren(Object parentElement) {
 		if (result == null) {
 			return IModelElement.NO_CHILDREN;
+		}
+		if (parentElement instanceof IFile) {
+			parentElement = BeansCorePlugin.getModel().getConfig((IFile) parentElement);
 		}
 
 		// Create list of matched element's children which belong to given
 		// parent element
 		Object[] matches = result.getElements();
-		List<IModelElement> children = new ArrayList<IModelElement>();
-		for (Object element0 : matches) {
-			IModelElement element = (IModelElement) element0;
-			IModelElement child = BeansModelUtils.getChildForElement(
-					(IModelElement) parentElement, element);
-			if (child != null) {
-				if (child instanceof IBeansConfig) {
-					
-				}
-				if (!children.contains(child)) {
-					children.add(child);
-				}
+		List<Object> children = new ArrayList<Object>();
+		for (Object match : matches) {
+			Object child = BeansModelUtils.getChildForElement(
+					(IModelElement) parentElement, (IModelElement) match);
+			if (child instanceof IBeansConfig) {
+				child = ((IBeansConfig) child).getElementResource();
+			}
+			if (child != null && !children.contains(child)) {
+				children.add(child);
 			}
 		}
-		return children.toArray(new IModelElement[children.size()]);
+		return children.toArray(new Object[children.size()]);
 	}
 
 	public void elementsChanged(Object[] elements) {
