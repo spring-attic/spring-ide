adb6544f47fd42bcf6486ca7aaa0720e04dacbdd
Torsten Juergeleit <torsten@springide.org>
2007-04-26 23:20:05 +0000

    moved WebFlow namespace label provider to webflow.ui plugin

---
+++ org.springframework.ide.eclipse.webflow.ui/src/org/springframework/ide/eclipse/webflow/ui/namespaces/WebflowLabelProvider.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2007 Spring IDE Developers
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Spring IDE Developers - initial API and implementation
+ *******************************************************************************/
+package org.springframework.ide.eclipse.webflow.ui.namespaces;
+
+import org.eclipse.swt.graphics.Image;
+import org.springframework.ide.eclipse.beans.core.model.IBean;
+import org.springframework.ide.eclipse.beans.core.model.IBeansConfig;
+import org.springframework.ide.eclipse.beans.core.model.IBeansConfigSet;
+import org.springframework.ide.eclipse.beans.ui.namespaces.DefaultNamespaceLabelProvider;
+import org.springframework.ide.eclipse.core.model.IModelElement;
+import org.springframework.ide.eclipse.core.model.ISourceModelElement;
+import org.springframework.ide.eclipse.webflow.ui.editor.namespaces.webflow.WebflowUIImages;
+
+/**
+ * @author Christian Dupuis
+ * @since 2.0
+ */
+public class WebflowLabelProvider extends DefaultNamespaceLabelProvider {
+
+	@Override
+	public Image getImage(ISourceModelElement element, IModelElement context) {
+		if (element instanceof IBean
+				&& (context instanceof IBeansConfig
+						|| context instanceof IBeansConfigSet)) {
+			return WebflowUIImages.getImage(WebflowUIImages.IMG_OBJS_WEBFLOW);
+		}
+		return super.getImage(element, context);
+	}
+}
