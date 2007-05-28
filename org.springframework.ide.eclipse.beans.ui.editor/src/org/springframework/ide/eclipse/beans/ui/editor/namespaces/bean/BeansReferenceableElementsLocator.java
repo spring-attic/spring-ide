908a1f3578df545e4fecd7ba31fa2b2cc073152b
Christian Dupuis <cdupuis@vmware.com>
2007-05-28 19:06:20 +0000

    added support for name attribute in content assist and navigation [fixes #606]

--- org.springframework.ide.eclipse.beans.ui.editor/src/org/springframework/ide/eclipse/beans/ui/editor/namespaces/bean/BeansReferenceableElementsLocator.java
+++ org.springframework.ide.eclipse.beans.ui.editor/src/org/springframework/ide/eclipse/beans/ui/editor/namespaces/bean/BeansReferenceableElementsLocator.java
@@ -15,6 +15,7 @@ import java.util.Map;
 
 import org.springframework.ide.eclipse.beans.ui.editor.namespaces.IReferenceableElementsLocator;
 import org.springframework.ide.eclipse.beans.ui.editor.util.BeansEditorUtils;
+import org.springframework.util.StringUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -22,15 +23,27 @@ import org.w3c.dom.NodeList;
 public class BeansReferenceableElementsLocator implements
 		IReferenceableElementsLocator {
 
+	public static final String BEAN_NAME_DELIMITERS = ",; ";
+
 	public Map<String, Node> getReferenceableElements(Document document) {
 		Map<String, Node> nodes = new HashMap<String, Node>();
 		NodeList childNodes = document.getDocumentElement().getChildNodes();
 
 		for (int i = 0; i < childNodes.getLength(); i++) {
 			Node node = childNodes.item(i);
-			if ("bean".equals(node.getNodeName())
-					&& BeansEditorUtils.hasAttribute(node, "id")) {
-				nodes.put(BeansEditorUtils.getAttribute(node, "id"), node);
+			if ("bean".equals(node.getNodeName())) {
+				if (BeansEditorUtils.hasAttribute(node, "id")) {
+					nodes.put(BeansEditorUtils.getAttribute(node, "id"), node);
+				}
+				if (BeansEditorUtils.hasAttribute(node, "name")) {
+					String aliasesString = BeansEditorUtils.getAttribute(node,
+							"name");
+					String[] nameArr = StringUtils.tokenizeToStringArray(
+							aliasesString, BEAN_NAME_DELIMITERS);
+					for (String name : nameArr) {
+						nodes.put(name, node);
+					}
+				}
 			}
 		}
 		return nodes;
