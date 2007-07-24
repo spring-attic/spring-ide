3be6912ea5106252f8a30321a16723efca4c404e
Christian Dupuis <cdupuis@vmware.com>
2007-07-24 18:45:13 +0000

    started to seperate features

--- org.springframework.ide.eclipse.build-pde/localBuild.sh
+++ org.springframework.ide.eclipse.build-pde/localBuild.sh
@@ -3,4 +3,4 @@
 # directory 
 WORKSPACE=`pwd`
 NAME=`date +%Y%m%d%H%M`
-$JAVA_HOME/bin/java -jar org.eclipse.releng.basebuilder/eclipse/startup.jar -application org.eclipse.ant.core.antRunner -buildfile $WORKSPACE/org.eclipse.releng.basebuilder/eclipse/plugins/org.eclipse.pde.build_3.2.0.v20060505a/scripts/build.xml -Dbuilder=$WORKSPACE/springide.builder -DforceContextQualifier=v${NAME} -Djava.rt=/System/Library/Frameworks/JavaVM.framework/Versions/1.5.0/Classes/classes.jar $@
+$JAVA_HOME/bin/java -jar org.eclipse.releng.basebuilder/eclipse/startup.jar -application org.eclipse.ant.core.antRunner -buildfile $WORKSPACE/org.eclipse.releng.basebuilder/eclipse/plugins/org.eclipse.pde.build_3.2.0.v20060505a/scripts/build.xml -Dbuilder=$WORKSPACE/beans-feature.builder -DforceContextQualifier=v${NAME} -Djava.rt=/System/Library/Frameworks/JavaVM.framework/Versions/1.5.0/Classes/classes.jar $@
