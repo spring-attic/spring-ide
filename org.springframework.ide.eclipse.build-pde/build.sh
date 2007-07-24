65ad3e0f59770fc95290cb0c05a04c07d21ef18c
Christian Dupuis <cdupuis@vmware.com>
2007-07-24 19:19:41 +0000

    removed javaconfig feature from beans feature

--- org.springframework.ide.eclipse.build-pde/build.sh
+++ org.springframework.ide.eclipse.build-pde/build.sh
@@ -5,3 +5,4 @@
 WORKSPACE=`pwd`
 NAME=`date +%Y%m%d%H%M`
 $JAVA_HOME/bin/java -jar org.eclipse.releng.basebuilder/eclipse/startup.jar -application org.eclipse.ant.core.antRunner -buildfile $WORKSPACE/org.eclipse.releng.basebuilder/eclipse/plugins/org.eclipse.pde.build_3.2.0.v20060505a/scripts/build.xml -Dbuilder=$WORKSPACE/beans-feature.builder -DforceContextQualifier=v${NAME} $@
+# $JAVA_HOME/bin/java -jar org.eclipse.releng.basebuilder/eclipse/startup.jar -application org.eclipse.ant.core.antRunner -buildfile $WORKSPACE/org.eclipse.releng.basebuilder/eclipse/plugins/org.eclipse.pde.build_3.2.0.v20060505a/scripts/build.xml -Dbuilder=$WORKSPACE/aop-feature.builder -DforceContextQualifier=v${NAME} $@
