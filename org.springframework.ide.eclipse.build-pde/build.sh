59700979163b63965c7e73cd3ed857758176072b
Christian Dupuis <cdupuis@vmware.com>
2007-09-11 16:37:32 +0000

    polished build; factored out build method in main build shell script

--- org.springframework.ide.eclipse.build-pde/build.sh
+++ org.springframework.ide.eclipse.build-pde/build.sh
@@ -5,40 +5,26 @@
 WORKSPACE=`pwd`
 NAME=`date +%Y%m%d%H%M`
 
+build() {
+    b=$1
+    shift
+    p=$@
+    echo Building $b with $p
+    $JAVA_HOME/bin/java -jar org.eclipse.releng.basebuilder/eclipse/startup.jar -application org.eclipse.ant.core.antRunner -buildfile $WORKSPACE/org.eclipse.releng.basebuilder/eclipse/plugins/org.eclipse.pde.build_3.2.0.v20060505a/scripts/build.xml -Dbuilder=$WORKSPACE/$b.builder -DforceContextQualifier=v${NAME} $p
+    if [ $? -ne 0 ]
+    then
+        exit 1
+    fi
+
+}
+
 echo Command line: $@
+build beans-feature $@
+build aop-feature $@
+build ajdt-feature $@
+build javaconfig-feature $@
+build webflow-feature $@
+build mylyn-feature $@
 
-$JAVA_HOME/bin/java -jar org.eclipse.releng.basebuilder/eclipse/startup.jar -application org.eclipse.ant.core.antRunner -buildfile $WORKSPACE/org.eclipse.releng.basebuilder/eclipse/plugins/org.eclipse.pde.build_3.2.0.v20060505a/scripts/build.xml -Dbuilder=$WORKSPACE/beans-feature.builder -DforceContextQualifier=v${NAME} $@
-if [ $? -ne 0 ]
-then
-        exit 1
-fi
 
-$JAVA_HOME/bin/java -jar org.eclipse.releng.basebuilder/eclipse/startup.jar -application org.eclipse.ant.core.antRunner -buildfile $WORKSPACE/org.eclipse.releng.basebuilder/eclipse/plugins/org.eclipse.pde.build_3.2.0.v20060505a/scripts/build.xml -Dbuilder=$WORKSPACE/aop-feature.builder -DforceContextQualifier=v${NAME} $@
-if [ $? -ne 0 ]
-then
-        exit 1
-fi
 
-$JAVA_HOME/bin/java -jar org.eclipse.releng.basebuilder/eclipse/startup.jar -application org.eclipse.ant.core.antRunner -buildfile $WORKSPACE/org.eclipse.releng.basebuilder/eclipse/plugins/org.eclipse.pde.build_3.2.0.v20060505a/scripts/build.xml -Dbuilder=$WORKSPACE/ajdt-feature.builder -DforceContextQualifier=v${NAME} $@
-if [ $? -ne 0 ]
-then
-        exit 1
-fi
-
-$JAVA_HOME/bin/java -jar org.eclipse.releng.basebuilder/eclipse/startup.jar -application org.eclipse.ant.core.antRunner -buildfile $WORKSPACE/org.eclipse.releng.basebuilder/eclipse/plugins/org.eclipse.pde.build_3.2.0.v20060505a/scripts/build.xml -Dbuilder=$WORKSPACE/javaconfig-feature.builder -DforceContextQualifier=v${NAME} $@
-if [ $? -ne 0 ]
-then
-        exit 1
-fi
-
-$JAVA_HOME/bin/java -jar org.eclipse.releng.basebuilder/eclipse/startup.jar -application org.eclipse.ant.core.antRunner -buildfile $WORKSPACE/org.eclipse.releng.basebuilder/eclipse/plugins/org.eclipse.pde.build_3.2.0.v20060505a/scripts/build.xml -Dbuilder=$WORKSPACE/webflow-feature.builder -DforceContextQualifier=v${NAME} $@
-if [ $? -ne 0 ]
-then
-        exit 1
-fi
-
-$JAVA_HOME/bin/java -jar org.eclipse.releng.basebuilder/eclipse/startup.jar -application org.eclipse.ant.core.antRunner -buildfile $WORKSPACE/org.eclipse.releng.basebuilder/eclipse/plugins/org.eclipse.pde.build_3.2.0.v20060505a/scripts/build.xml -Dbuilder=$WORKSPACE/mylyn-feature.builder -DforceContextQualifier=v${NAME} $@
-if [ $? -ne 0 ]
-then
-        exit 1
-fi
