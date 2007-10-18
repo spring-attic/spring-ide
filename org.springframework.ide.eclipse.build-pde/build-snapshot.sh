4b4ccbeecace65468b4332d14ac3811c3d831258
Christian Dupuis <cdupuis@vmware.com>
2007-10-18 09:22:36 +0000

    prepared for weekly build

--- org.springframework.ide.eclipse.build-pde/build-snapshot.sh
+++ org.springframework.ide.eclipse.build-pde/build-snapshot.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 WORKSPACE=`pwd`
-./build.sh $@
+REMOTE_PATH=$1
+shift
+ARGS=$@
+
+./build.sh $ARGS
 if [ $? -ne 0 ]
 then
     exit 1
 fi
 
-scp -r $WORKSPACE/../updatesite_nightly/* bamboo@springide.org:/home/springide/htdocs/updatesite_nightly
+scp -r $WORKSPACE/../updatesite_nightly/* bamboo@springide.org:/home/springide/htdocs/$REMOTE_PATH
