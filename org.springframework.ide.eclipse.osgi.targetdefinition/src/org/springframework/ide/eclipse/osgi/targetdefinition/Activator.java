63e92ba81e3f38da554d87248c842f257ff913f3
Christian Dupuis <cdupuis@vmware.com>
2008-01-02 22:49:07 +0000

    IDE-778: first version of Spring DM target runtime

---
+++ org.springframework.ide.eclipse.osgi.targetdefinition/src/org/springframework/ide/eclipse/osgi/targetdefinition/Activator.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2007 Spring IDE Developers
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Spring IDE Developers - initial API and implementation
+ *******************************************************************************/
+package org.springframework.ide.eclipse.osgi.targetdefinition;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ * @author Christian Dupuis
+ * @since 2.0.3
+ */
+public class Activator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = 
+		"org.springframework.ide.eclipse.osgi.targetdefinition";
+
+	// The shared instance
+	private static Activator plugin;
+	
+	public Activator() {
+	}
+
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+}
---
+++ org.springframework.ide.eclipse.osgi.targetdefinition/targets/release/download_sources.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+SPRING_VERSION=2.5
+BUNDLE_SPRING_VERSION=2.5.0
+
+OSGI_VERSION=1.0-rc1
+BUNDLE_OSGI_VERSION=1.0.0.rc1
+
+download_source() {	
+	GROUP_ID=$1
+	ARTIFACT_ID=$2
+	VERSION=$3
+	SYMBOLIC_NAME=$4
+	BUNDLE_VERSION=$5
+	SRC_BUNDLE=$6
+	REPO=$7
+	FOLDER_NAME=./target/plugins/${SRC_BUNDLE}_${BUNDLE_VERSION}/src/${SYMBOLIC_NAME}_${BUNDLE_VERSION}
+
+	mkdir $FOLDER_NAME
+	curl -o $FOLDER_NAME/src.zip ${REPO}/$GROUP_ID/$ARTIFACT_ID/$VERSION/$ARTIFACT_ID-$VERSION-sources.jar 
+}
+
+download_spring_source() {
+	download_source org/springframework $1 $SPRING_VERSION $2 $BUNDLE_SPRING_VERSION org.springframework.spring.bundle.source http://repo1.maven.org/maven2
+}
+
+download_osgi_source() {
+	download_source org/springframework/osgi $1 $OSGI_VERSION $2 $BUNDLE_OSGI_VERSION org.springframework.osgi.bundle.source http://s3.amazonaws.com/maven.springframework.org/milestone
+}
+
+download_spring_source spring-aop org.springframework.bundle.spring.aop 
+download_spring_source spring-beans org.springframework.bundle.spring.beans
+download_spring_source spring-core org.springframework.bundle.spring.core
+download_spring_source spring-context org.springframework.bundle.spring.context 
+download_spring_source spring-context-support org.springframework.bundle.spring.context.support 
+
+download_osgi_source spring-osgi-annotation org.springframework.osgi.bundle.annotation
+download_osgi_source spring-osgi-core org.springframework.osgi.bundle.core
+download_osgi_source spring-osgi-extender org.springframework.osgi.bundle.extender
+download_osgi_source spring-osgi-io org.springframework.osgi.bundle.io
+download_osgi_source spring-osgi-test org.springframework.osgi.bundle.test
