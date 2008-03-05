f4be88ddb2569394bf0984ba35eeb9217141b1b3
Christian Dupuis <cdupuis@vmware.com>
2008-03-05 13:16:42 +0000

    fixed test case

--- org.springframework.ide.eclipse.beans.core.tests/src/org/springframework/ide/eclipse/core/java/IntrospectorTest.java
+++ org.springframework.ide.eclipse.beans.core.tests/src/org/springframework/ide/eclipse/core/java/IntrospectorTest.java
@@ -27,12 +27,12 @@ import org.springframework.ide.eclipse.core.java.Introspector.Static;
  * @since 2.0.3
  */
 public class IntrospectorTest extends BeansCoreTestCase {
-	
+
 	/**
-	 * Several tests dealing with intertype declared methods on the bean class 
+	 * Several tests dealing with intertype declared methods on the bean class
 	 */
 	public void testDeclaredMethods() throws CoreException, IOException {
-		IProject project = createPredefinedProject("aspectj"); 		
+		IProject project = createPredefinedProject("aspectj");
 		IType foo = JdtUtils.getJavaType(project, "org.springframework.Foo");
 		Set<IMethod> methods = Introspector.findAllWritableProperties(foo);
 		assertTrue(!methods.isEmpty() && methods.toArray().length > 1);
@@ -41,75 +41,79 @@ public class IntrospectorTest extends BeansCoreTestCase {
 		assertNotNull(Introspector.getReadableProperty(foo, "bar"));
 		assertTrue(Introspector.hasWritableProperty(foo, "foochen"));
 		methods = Introspector.findAllNoParameterMethods(foo, "getBar");
-		assertTrue(!methods.isEmpty() && methods.toArray().length == 1);  
+		assertTrue(!methods.isEmpty() && methods.toArray().length == 1);
 	}
 
 	public void testDoesExtend() throws CoreException, IOException {
-		IProject project = createPredefinedProject("validation"); 		
+		IProject project = createPredefinedProject("validation");
 		IType foo = JdtUtils.getJavaType(project, "org.springframework.SubClass");
 		assertTrue(Introspector.doesExtend(foo, "org.springframework.Base"));
 		assertTrue(!Introspector.doesExtend(foo, "org.springframework.beans.factory.BeanFactory"));
 	}
 
 	public void testDoesImplement() throws CoreException, IOException {
-		IProject project = createPredefinedProject("validation"); 		
+		IProject project = createPredefinedProject("validation");
 		IType foo = JdtUtils.getJavaType(project, "org.springframework.SubClass");
 		assertTrue(Introspector.doesImplement(foo, "org.springframework.FooInterface"));
-		assertTrue(!Introspector.doesImplement(foo, "org.springframework.beans.factory.BeanFactory"));
+		assertTrue(!Introspector
+				.doesImplement(foo, "org.springframework.beans.factory.BeanFactory"));
 		IType base = JdtUtils.getJavaType(project, "org.springframework.Base");
 		assertTrue(Introspector.doesImplement(base, "org.springframework.FooInterface"));
 	}
 
 	public void testfindAllConstructor() throws CoreException, IOException {
-		IProject project = createPredefinedProject("validation"); 		
+		IProject project = createPredefinedProject("validation");
 		IType foo = JdtUtils.getJavaType(project, "org.springframework.SubClass");
 		Set<IMethod> cons = Introspector.findAllConstructors(foo);
 		assertTrue(!cons.isEmpty());
 		assertTrue(cons.toArray().length == 3);
 	}
-	
+
 	public void testFindAllMethodsWithFilter() throws CoreException, IOException {
-		IProject project = createPredefinedProject("validation"); 		
+		IProject project = createPredefinedProject("validation");
 		IType foo = JdtUtils.getJavaType(project, "org.springframework.SubClass");
 		Set<IMethod> methods = Introspector.findAllMethods(foo, new IMethodFilter() {
 
 			public boolean matches(IMethod method, String prefix) {
 				return true;
-			}});
+			}
+		});
 		assertTrue(!methods.isEmpty());
-		assertTrue(methods.toArray().length == 8);
+		checkResult(methods, 11);
 
 		methods = Introspector.findAllMethods(foo, "set", new IMethodFilter() {
-			
+
 			public boolean matches(IMethod method, String prefix) {
 				return (method.getElementName().startsWith(prefix));
-			}});
+			}
+		});
 		assertTrue(!methods.isEmpty());
-		assertTrue(methods.toArray().length == 3);
-	} 
+		checkResult(methods, 3);
+	}
 
 	public void testFindAllMethods() throws CoreException, IOException {
-		IProject project = createPredefinedProject("validation"); 		
+		IProject project = createPredefinedProject("validation");
 		IType foo = JdtUtils.getJavaType(project, "org.springframework.SubClass");
 		// only methods; no constructors
-		Set<IMethod> methods = Introspector.findAllMethods(foo, "", -1, Public.DONT_CARE, Static.DONT_CARE);
-		assertTrue(methods.toArray().length == 8);
+		Set<IMethod> methods = Introspector.findAllMethods(foo, "", -1, Public.DONT_CARE,
+				Static.DONT_CARE);
+		checkResult(methods, 11);
 		// only public setters (static does not matter)
 		methods = Introspector.findAllMethods(foo, "set", 1, Public.YES, Static.DONT_CARE);
-		assertTrue(methods.toArray().length == 3);
+		checkResult(methods, 3);
 		// only public setters (non-static)
 		methods = Introspector.findAllMethods(foo, "set", 1, Public.YES, Static.NO);
-		assertTrue(methods.toArray().length == 2);
+		checkResult(methods, 2);
 		// only protected methods setters (static does not matter)
 		methods = Introspector.findAllMethods(foo, "", -1, Public.NO, Static.DONT_CARE);
-		assertTrue(methods.toArray().length == 2);
+		checkResult(methods, 5);
 		// only protected methods setters (static)
 		methods = Introspector.findAllMethods(foo, "", -1, Public.NO, Static.YES);
-		assertTrue(methods.toArray().length == 1);
-	} 
-	
+		checkResult(methods, 2);
+	}
+
 	public void testGetAllImplementedInterfaces() throws CoreException, IOException {
-		IProject project = createPredefinedProject("validation"); 		
+		IProject project = createPredefinedProject("validation");
 		IType foo = JdtUtils.getJavaType(project, "org.springframework.SubClass");
 		Set<IType> interfaces = Introspector.getAllImplementedInterfaces(foo);
 		assertTrue(interfaces.toArray().length == 2);
@@ -119,13 +123,18 @@ public class IntrospectorTest extends BeansCoreTestCase {
 	}
 
 	public void testGetAllMethods() throws CoreException, IOException {
-		IProject project = createPredefinedProject("validation"); 		
+		IProject project = createPredefinedProject("validation");
 		IType foo = JdtUtils.getJavaType(project, "org.springframework.SubClass");
 		Set<IMethod> methods = Introspector.getAllMethods(foo);
-		assertTrue(methods.toArray().length == 8);
+		checkResult(methods, 11);
 		IType base = JdtUtils.getJavaType(project, "org.springframework.Base");
 		methods = Introspector.getAllMethods(base);
-		assertTrue(methods.toArray().length == 4);
+		checkResult(methods, 7);
 	}
-	
+
+	private void checkResult(Set<IMethod> methods, int expectedSize) {
+		assertTrue("Expected " + expectedSize + " methods to be found. actual is: "
+				+ methods.toArray().length, methods.toArray().length == expectedSize);
+	}
+
 }
