429eb6cd2c4a80b93ce53c7721719d4f3149bf9b
Christian Dupuis <cdupuis@vmware.com>
2008-04-25 14:16:27 +0000

    IDE-849: bean names are now generated with using the correct file name in case of imported resources; the name of the importing resource is not used anymore preventing problems with duplicate bean names

--- org.springframework.ide.eclipse.beans.core/src/org/springframework/ide/eclipse/beans/core/internal/model/UniqueBeanNameGenerator.java
+++ org.springframework.ide.eclipse.beans.core/src/org/springframework/ide/eclipse/beans/core/internal/model/UniqueBeanNameGenerator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2007 Spring IDE Developers
+ * Copyright (c) 2005, 2008 Spring IDE Developers
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,6 +10,8 @@
  *******************************************************************************/
 package org.springframework.ide.eclipse.beans.core.internal.model;
 
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IAdaptable;
 import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
@@ -19,12 +21,11 @@ import org.springframework.ide.eclipse.core.model.IModelSourceLocation;
 import org.springframework.ide.eclipse.core.model.ModelUtils;
 
 /**
- * {@link BeanNameGenerator} which creates a bean name which is unique within
- * the beans core model. This name consists of the bean class or parent name or
- * object identity, the project name, the config file name and the start line
- * number delimited by '#'.
- * 
+ * {@link BeanNameGenerator} which creates a bean name which is unique within the beans core model.
+ * This name consists of the bean class or parent name or object identity, the project name, the
+ * config file name and the start line number delimited by '#'.
  * @author Torsten Juergeleit
+ * @author Christian Dupuis
  * @since 2.0
  */
 public class UniqueBeanNameGenerator implements BeanNameGenerator {
@@ -35,14 +36,12 @@ public class UniqueBeanNameGenerator implements BeanNameGenerator {
 		this.config = config;
 	}
 
-	public String generateBeanName(BeanDefinition definition,
-			BeanDefinitionRegistry registry) {
+	public String generateBeanName(BeanDefinition definition, BeanDefinitionRegistry registry) {
 		return generateBeanName(definition, config);
 	}
 
-	public static String generateBeanName(BeanDefinition definition,
-			IBeansConfig config) {
-		StringBuffer name = new StringBuffer();
+	public static String generateBeanName(BeanDefinition definition, IBeansConfig config) {
+		StringBuilder name = new StringBuilder();
 		if (definition.getBeanClassName() != null) {
 			name.append(definition.getBeanClassName());
 		}
@@ -52,23 +51,43 @@ public class UniqueBeanNameGenerator implements BeanNameGenerator {
 				name.append("$child");
 			}
 			else if (definition.getFactoryBeanName() != null) {
-				name.append(definition
-						.getFactoryBeanName());
+				name.append(definition.getFactoryBeanName());
 				name.append("$created");
-			} else {
+			}
+			else {
 				name.append("!!!invalid name!!!");
 			}
 		}
-		IModelSourceLocation location = ModelUtils
-				.getSourceLocation(definition);
+		IModelSourceLocation location = ModelUtils.getSourceLocation(definition);
 		if (location != null) {
 			name.append(BeanFactoryUtils.GENERATED_BEAN_NAME_SEPARATOR);
 			name.append(config.getElementParent().getElementName());
 			name.append(BeanFactoryUtils.GENERATED_BEAN_NAME_SEPARATOR);
-			name.append(config.getElementName());
+
+			// Make sure file name actually comes from the correct resource and always from the
+			// passed config. Important in case of imported beans configs.
+			name.append(generateFilename(config, location));
+
 			name.append(BeanFactoryUtils.GENERATED_BEAN_NAME_SEPARATOR);
 			name.append(location.getStartLine());
 		}
 		return name.toString();
 	}
+	
+	/**
+	 * Returns the name of the originating file that contains the bean definition.
+	 * @since 2.0.5
+	 */
+	private static String generateFilename(IBeansConfig config, IModelSourceLocation location) {
+		String fileName = config.getElementName();
+		if (location.getResource() instanceof IAdaptable) {
+			IResource resource = (IResource) ((IAdaptable) location.getResource())
+					.getAdapter(IResource.class);
+			if (resource != null) {
+				fileName = resource.getProjectRelativePath().toString();
+			}
+		}
+		return fileName;
+	}
+
 }
