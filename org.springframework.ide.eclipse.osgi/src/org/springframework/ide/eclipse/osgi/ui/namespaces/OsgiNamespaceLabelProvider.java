0249fe474c1a6dbfda8521dd4a45d3eaaca77990
Christian Dupuis <cdupuis@vmware.com>
2009-03-15 14:45:08 +0000

    polishing

--- org.springframework.ide.eclipse.osgi/src/org/springframework/ide/eclipse/osgi/ui/namespaces/OsgiNamespaceLabelProvider.java
+++ org.springframework.ide.eclipse.osgi/src/org/springframework/ide/eclipse/osgi/ui/namespaces/OsgiNamespaceLabelProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2007 Spring IDE Developers
+ * Copyright (c) 2005, 2009 Spring IDE Developers
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -21,7 +21,7 @@ import org.springframework.ide.eclipse.osgi.OsgiUIImages;
  * @author Christian Dupuis
  * @since 2.0.1
  */
-public class OsgiConfigNamespaceLabelProvider extends DefaultNamespaceLabelProvider {
+public class OsgiNamespaceLabelProvider extends DefaultNamespaceLabelProvider {
 
 	@Override
 	public Image getImage(ISourceModelElement element, IModelElement context, boolean isDecorating) {
