97696789b3cf2983da029724f2684d78e7b69e08
Christian Dupuis <cdupuis@vmware.com>
2009-07-27 13:03:29 +0000

    polishing

--- org.springframework.ide.eclipse.core/src/org/springframework/ide/eclipse/core/java/JdtUtils.java
+++ org.springframework.ide.eclipse.core/src/org/springframework/ide/eclipse/core/java/JdtUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2008 Spring IDE Developers
+ * Copyright (c) 2005, 2009 Spring IDE Developers
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -648,6 +648,9 @@ public class JdtUtils {
 	}
 
 	public static String resolveClassName(String className, IType type) {
+		if (className == null || type == null) {
+			return className;
+		}
 		// replace binary $ inner class name syntax with . for source level
 		className = className.replace('$', '.');
 		String dotClassName = new StringBuilder().append('.').append(className).toString();
