eafa30894220f6b87f8594d0452ca368a03f91fc
Christian Dupuis <cdupuis@vmware.com>
2009-11-08 15:21:50 +0000

    polishing

--- org.springframework.ide.eclipse.core/src/org/springframework/ide/eclipse/core/io/xml/LineNumberPreservingDOMParser.java
+++ org.springframework.ide.eclipse.core/src/org/springframework/ide/eclipse/core/io/xml/LineNumberPreservingDOMParser.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2007 Spring IDE Developers
+ * Copyright (c) 2005, 2009 Spring IDE Developers
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -23,14 +23,16 @@ import org.w3c.dom.UserDataHandler;
 import org.xml.sax.SAXException;
 
 /**
- * Extended version of Xerces' DOM parser which adds line numbers
- * (as DOM level 3 user data) to every node.
- * <p><b>Requires Xerces 2.7 or newer!!!</b></p>
+ * Extended version of Xerces' DOM parser which adds line numbers (as DOM level 3 user data) to every node.
+ * <p>
+ * <b>Requires Xerces 2.7 or newer!!!</b>
+ * </p>
  * @author Torsten Juergeleit
  */
 public class LineNumberPreservingDOMParser extends DOMParser {
 
 	private static final String START_LINE = "startLine";
+
 	private static final String END_LINE = "endLine";
 
 	private XMLLocator locator;
@@ -54,7 +56,8 @@ public class LineNumberPreservingDOMParser extends DOMParser {
 			if (line != null && line.length() > 0) {
 				try {
 					return Integer.parseInt(line);
-				} catch (NumberFormatException e) {
+				}
+				catch (NumberFormatException e) {
 					// ignore invalid user data
 				}
 			}
@@ -63,8 +66,7 @@ public class LineNumberPreservingDOMParser extends DOMParser {
 	}
 
 	@Override
-	public void startDocument(XMLLocator locator, String encoding,
-			NamespaceContext namespaceContext, Augmentations augs)
+	public void startDocument(XMLLocator locator, String encoding, NamespaceContext namespaceContext, Augmentations augs)
 			throws XNIException {
 		this.locator = locator;
 		super.startDocument(locator, encoding, namespaceContext, augs);
@@ -78,15 +80,13 @@ public class LineNumberPreservingDOMParser extends DOMParser {
 	}
 
 	@Override
-	public void startElement(QName element, XMLAttributes attributes,
-			Augmentations augs) throws XNIException {
+	public void startElement(QName element, XMLAttributes attributes, Augmentations augs) throws XNIException {
 		super.startElement(element, attributes, augs);
 		addLineNumberToCurrentNode(START_LINE);
 	}
 
 	@Override
-	public void endElement(QName element, Augmentations augs)
-			throws XNIException {
+	public void endElement(QName element, Augmentations augs) throws XNIException {
 		addLineNumberToCurrentNode(END_LINE);
 		super.endElement(element, augs);
 	}
@@ -96,10 +96,10 @@ public class LineNumberPreservingDOMParser extends DOMParser {
 			Node node = (Node) getProperty(CURRENT_ELEMENT_NODE);
 			if (node instanceof NodeImpl) {
 				String line = String.valueOf(locator.getLineNumber());
-				((NodeImpl) node)
-						.setUserData(key, line, (UserDataHandler) null);
+				((NodeImpl) node).setUserData(key, line, (UserDataHandler) null);
 			}
-		} catch (SAXException e) {
+		}
+		catch (SAXException e) {
 			throw new XNIException(e);
 		}
 	}
