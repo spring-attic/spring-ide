1016ec3f40c2347c64b0d06cfd484be42ef3c77a
Christian Dupuis <cdupuis@vmware.com>
2010-01-06 13:57:25 +0000

    updated beans labels for Spring 3.0

--- org.springframework.ide.eclipse.beans.ui/src/org/springframework/ide/eclipse/beans/ui/model/BeansModelLabels.java
+++ org.springframework.ide.eclipse.beans.ui/src/org/springframework/ide/eclipse/beans/ui/model/BeansModelLabels.java
@@ -31,8 +31,7 @@ import org.springframework.ide.eclipse.core.model.ISourceModelElement;
 import org.springframework.util.StringUtils;
 
 /**
- * This class provides labels for the beans core model's
- * {@link IModelElement elements}.
+ * This class provides labels for the beans core model's {@link IModelElement elements}.
  * 
  * @author Torsten Juergeleit
  */
@@ -44,17 +43,18 @@ public final class BeansModelLabels extends BeansUILabels {
 		return buf.toString();
 	}
 
-	public static void appendElementLabel(IModelElement element, int flags,
-			StringBuffer buf) {
+	public static void appendElementLabel(IModelElement element, int flags, StringBuffer buf) {
 		if (isFlagged(flags, PREPEND_PATH)) {
 			appendElementPathLabel(element, flags, buf);
 			buf.append(CONCAT_STRING);
 		}
 		if (element instanceof IBeansConfig) {
 			appendBeansConfigLabel((IBeansConfig) element, flags, buf);
-		} else if (element instanceof ISourceModelElement) {
+		}
+		else if (element instanceof ISourceModelElement) {
 			appendElementLabel((ISourceModelElement) element, buf);
-		} else {
+		}
+		else {
 			buf.append(element.getElementName());
 		}
 		if (isFlagged(flags, APPEND_PATH)) {
@@ -63,31 +63,25 @@ public final class BeansModelLabels extends BeansUILabels {
 		}
 	}
 
-	public static void appendElementPathLabel(IModelElement element,
-			int flags, StringBuffer buf) {
+	public static void appendElementPathLabel(IModelElement element, int flags, StringBuffer buf) {
 		if (element instanceof IResourceModelElement) {
-			IResource resource = ((IResourceModelElement) element)
-					.getElementResource();
+			IResource resource = ((IResourceModelElement) element).getElementResource();
 			String path;
-			if (element instanceof IBeansConfig
-					&& isFlagged(flags, DESCRIPTION)) {
-				path = resource.getFullPath().makeRelative()
-						.removeLastSegments(1).toString();
+			if (element instanceof IBeansConfig && isFlagged(flags, DESCRIPTION)) {
+				path = resource.getFullPath().makeRelative().removeLastSegments(1).toString();
 			}
 			else {
 				path = resource.getProjectRelativePath().makeRelative().toString();
 			}
 			buf.append(path);
-			if (element instanceof IBeanConstructorArgument
-					|| element instanceof IBeanProperty) {
+			if (element instanceof IBeanConstructorArgument || element instanceof IBeanProperty) {
 				buf.append(CONCAT_STRING);
 				buf.append(element.getElementParent().getElementName());
 			}
 		}
 	}
 
-	public static void appendBeansConfigLabel(IBeansConfig config,
-			int flags, StringBuffer buf) {
+	public static void appendBeansConfigLabel(IBeansConfig config, int flags, StringBuffer buf) {
 		if (isFlagged(flags, DESCRIPTION)) {
 			String configName = config.getElementName();
 			if (config.isElementArchived()) {
@@ -95,43 +89,45 @@ public final class BeansModelLabels extends BeansUILabels {
 				buf.append(storage.getFullPath());
 				buf.append(" - ");
 				buf.append(storage.getFile().getName());
-			} else {
+			}
+			else {
 				buf.append(new Path(configName).lastSegment());
 			}
-		} else {
+		}
+		else {
 			buf.append("beans");
-			if (!config.getDefaultLazyInit()
-					.equals(IBeansConfig.DEFAULT_LAZY_INIT)) {
+			if (StringUtils.hasLength(config.getDefaultLazyInit())
+					&& !config.getDefaultLazyInit().equals(IBeansConfig.DEFAULT_LAZY_INIT)) {
 				buf.append(" lazy-init=\"");
 				buf.append(config.getDefaultLazyInit());
 				buf.append('"');
 			}
-			if (!config.getDefaultAutowire()
-					.equals(IBeansConfig.DEFAULT_AUTO_WIRE)) {
+			if (StringUtils.hasLength(config.getDefaultAutowire())
+					&& !config.getDefaultAutowire().equals(IBeansConfig.DEFAULT_AUTO_WIRE)) {
 				buf.append(" autowire=\"");
 				buf.append(config.getDefaultAutowire());
 				buf.append('"');
 			}
-			if (!config.getDefaultDependencyCheck()
-					.equals(IBeansConfig.DEFAULT_DEPENDENCY_CHECK)) {
+			if (StringUtils.hasLength(config.getDefaultDependencyCheck())
+					&& !config.getDefaultDependencyCheck().equals(IBeansConfig.DEFAULT_DEPENDENCY_CHECK)) {
 				buf.append(" dependency-check=\"");
 				buf.append(config.getDefaultDependencyCheck());
 				buf.append('"');
 			}
-			if (!config.getDefaultInitMethod()
-					.equals(IBeansConfig.DEFAULT_INIT_METHOD)) {
+			if (StringUtils.hasLength(config.getDefaultInitMethod())
+					&& !config.getDefaultInitMethod().equals(IBeansConfig.DEFAULT_INIT_METHOD)) {
 				buf.append(" init-method=\"");
 				buf.append(config.getDefaultInitMethod());
 				buf.append('"');
 			}
-			if (!config.getDefaultDestroyMethod()
-					.equals(IBeansConfig.DEFAULT_DESTROY_METHOD)) {
+			if (StringUtils.hasLength(config.getDefaultDestroyMethod())
+					&& !config.getDefaultDestroyMethod().equals(IBeansConfig.DEFAULT_DESTROY_METHOD)) {
 				buf.append(" destroy-method=");
 				buf.append(config.getDefaultDestroyMethod());
 				buf.append('"');
 			}
-			if (!config.getDefaultMerge()
-					.equals(IBeansConfig.DEFAULT_MERGE)) {
+			if (StringUtils.hasLength(config.getDefaultMerge())
+					&& !config.getDefaultMerge().equals(IBeansConfig.DEFAULT_MERGE)) {
 				buf.append(" merge=\"");
 				buf.append(config.getDefaultMerge());
 				buf.append('"');
@@ -145,19 +141,18 @@ public final class BeansModelLabels extends BeansUILabels {
 		}
 		if (bean.getAliases() != null && bean.getAliases().length > 0) {
 			buf.append('\'');
-			buf.append(StringUtils.arrayToDelimitedString(bean.getAliases(),
-					LIST_DELIMITER_STRING));
+			buf.append(StringUtils.arrayToDelimitedString(bean.getAliases(), LIST_DELIMITER_STRING));
 			buf.append('\'');
 		}
 		if (bean.getClassName() != null) {
 			buf.append('[').append(bean.getClassName()).append(']');
-		} else if (bean.getParentName() != null) {
+		}
+		else if (bean.getParentName() != null) {
 			buf.append('<').append(bean.getParentName()).append('>');
 		}
 	}
 
-	public static void appendElementLabel(IModelElement element,
-			StringBuffer buf) {
+	public static void appendElementLabel(IModelElement element, StringBuffer buf) {
 		if (element instanceof IBeansList) {
 			buf.append("list");
 		}
