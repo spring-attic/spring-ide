5e3b6dd52420ac9e26c26f9cc1c36a7fb28fbb74
Christian Dupuis <cdupuis@vmware.com>
2010-02-04 17:08:16 +0000

    added support Spring Batch 2.1

---
+++ org.springframework.ide.eclipse.batch/src/org/springframework/batch/core/configuration/v1/xml/CoreNamespaceHandler.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2006-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.batch.core.configuration.v1.xml;
+
+import org.springframework.batch.core.configuration.xml.JobParser;
+import org.springframework.batch.core.configuration.xml.JobRepositoryParser;
+import org.springframework.batch.core.configuration.xml.TopLevelJobListenerParser;
+import org.springframework.batch.core.configuration.xml.TopLevelStepListenerParser;
+import org.springframework.batch.core.configuration.xml.TopLevelStepParser;
+import org.springframework.beans.factory.xml.NamespaceHandler;
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+/**
+ * @author Dave Syer
+ */
+public class CoreNamespaceHandler extends NamespaceHandlerSupport {
+
+	/**
+	 * @see NamespaceHandler#init()
+	 */
+	public void init() {
+		this.registerBeanDefinitionParser("job", new JobParser());
+		this.registerBeanDefinitionParser("step", new TopLevelStepParser());
+		this.registerBeanDefinitionParser("job-repository", new JobRepositoryParser());
+		this.registerBeanDefinitionParser("job-listener", new TopLevelJobListenerParser());
+		this.registerBeanDefinitionParser("step-listener", new TopLevelStepListenerParser());
+	}
+}
