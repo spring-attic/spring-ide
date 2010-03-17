d9281682b73f7b658812388b54cdeb9a141bce8c
Christian Dupuis <cdupuis@vmware.com>
2010-03-17 17:47:00 +0000

    fixed typo

--- org.springframework.ide.eclipse.beans.ui.editor/src/org/springframework/ide/eclipse/beans/ui/editor/contentassist/context/ContextContentAssistProcessor.java
+++ org.springframework.ide.eclipse.beans.ui.editor/src/org/springframework/ide/eclipse/beans/ui/editor/contentassist/context/ContextContentAssistProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2007 Spring IDE Developers
+ * Copyright (c) 2005, 2010 Spring IDE Developers
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -41,6 +41,6 @@ public class ContextContentAssistProcessor extends
 		
 		ClassContentAssistCalculator clazz = new ClassContentAssistCalculator(false);
 		registerContentAssistCalculator("include-filter", "expression", clazz);
-		registerContentAssistCalculator("exclude", "expression", clazz);
+		registerContentAssistCalculator("exclude-filter", "expression", clazz);
 	}
 }
