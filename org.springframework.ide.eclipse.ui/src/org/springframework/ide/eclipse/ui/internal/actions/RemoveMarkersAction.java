e5703b96035a3f8f8f4274ff781430ea7a8cb569
Christian Dupuis <cdupuis@vmware.com>
2010-04-25 15:24:18 +0000

    added action to remove all Spring markers from projects

---
+++ org.springframework.ide.eclipse.ui/src/org/springframework/ide/eclipse/ui/internal/actions/RemoveMarkersAction.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2010 Spring IDE Developers
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Spring IDE Developers - initial API and implementation
+ *******************************************************************************/
+package org.springframework.ide.eclipse.ui.internal.actions;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.springframework.ide.eclipse.core.SpringCore;
+
+public class RemoveMarkersAction implements IObjectActionDelegate {
+
+	private List<IProject> selected = new ArrayList<IProject>();
+
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+		selected.clear();
+		if (selection instanceof IStructuredSelection) {
+			boolean enabled = true;
+			Iterator iter = ((IStructuredSelection) selection).iterator();
+			while (iter.hasNext()) {
+				Object obj = iter.next();
+				if (obj instanceof IJavaProject) {
+					obj = ((IJavaProject) obj).getProject();
+				}
+				if (obj instanceof IProject) {
+					IProject project = (IProject) obj;
+					if (!project.isOpen()) {
+						enabled = false;
+						break;
+					}
+					else {
+						selected.add(project);
+					}
+				}
+				else {
+					enabled = false;
+					break;
+				}
+			}
+			action.setEnabled(enabled);
+		}
+	}
+
+	public void run(IAction action) {
+		Iterator iter = selected.iterator();
+		while (iter.hasNext()) {
+			IProject project = (IProject) iter.next();
+			try {
+				for (IMarker marker : project.findMarkers(SpringCore.MARKER_ID, true, IResource.DEPTH_INFINITE)) {
+					marker.delete();
+				}
+			}
+			catch (CoreException e) {
+				SpringCore.log(e);
+			}
+		}
+	}
+}
