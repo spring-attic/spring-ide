79312d851351c5e75b4c7903bc9c34f1c6c1ad81
Christian Dupuis <cdupuis@vmware.com>
2010-10-14 08:45:42 +0000

    Changes to UAA plugin to query an update site to present updates to the Spring IDE components

--- org.springframework.ide.eclipse.uaa/src/org/springframework/ide/eclipse/internal/uaa/UaaPlugin.java
+++ org.springframework.ide.eclipse.uaa/src/org/springframework/ide/eclipse/internal/uaa/UaaPlugin.java
@@ -10,7 +10,9 @@
  *******************************************************************************/
 package org.springframework.ide.eclipse.internal.uaa;
 
+import java.net.URI;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.httpclient.params.DefaultHttpParams;
@@ -18,9 +20,20 @@ import org.apache.commons.httpclient.params.HttpClientParams;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.equinox.p2.core.IProvisioningAgent;
+import org.eclipse.equinox.p2.engine.IProfile;
+import org.eclipse.equinox.p2.engine.IProfileRegistry;
+import org.eclipse.equinox.p2.engine.ProvisioningContext;
+import org.eclipse.equinox.p2.metadata.IInstallableUnit;
+import org.eclipse.equinox.p2.planner.IPlanner;
+import org.eclipse.equinox.p2.query.IQuery;
+import org.eclipse.equinox.p2.query.IQueryResult;
+import org.eclipse.equinox.p2.query.QueryUtil;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.eclipse.ui.progress.UIJob;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 import org.springframework.ide.eclipse.internal.uaa.monitor.CommandUsageMonitor;
 import org.springframework.ide.eclipse.internal.uaa.monitor.LibraryUsageMonitor;
 import org.springframework.ide.eclipse.internal.uaa.monitor.PartUsageMonitor;
@@ -47,7 +60,7 @@ public class UaaPlugin extends AbstractUIPlugin {
 	private static UaaPlugin plugin;
 
 	private List<IUsageMonitor> monitors = new ArrayList<IUsageMonitor>();
-	
+
 	private UaaManager usageMonitorManager;
 
 	public int getPrivacyLevel() {
@@ -64,35 +77,75 @@ public class UaaPlugin extends AbstractUIPlugin {
 		}
 		return DefaultHttpParams.getDefaultParams().getParameter(HttpClientParams.USER_AGENT).toString();
 	}
-	
+
 	public void setPrivacyLevel(int level) {
 		this.usageMonitorManager.setPrivacyLevel(level);
 	}
-	
-	public void start(BundleContext context) throws Exception {
+
+	public void start(final BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
-		
+
 		monitors.clear();
 		monitors.add(new PartUsageMonitor());
 		monitors.add(new CommandUsageMonitor());
 		monitors.add(new LibraryUsageMonitor());
 		monitors.add(new ProjectContributionUsageMonitor());
-		
-		UIJob job = new UIJob("Initializing Spring UAA") { //$NON-NLS-1$
+
+		UIJob startupJob = new UIJob("Initializing Spring UAA") { //$NON-NLS-1$
 
 			public IStatus runInUIThread(IProgressMonitor progressMonitor) {
 				usageMonitorManager = new UaaManager();
 				for (IUsageMonitor monitor : monitors) {
 					monitor.startMonitoring(usageMonitorManager);
 				}
+
 				return Status.OK_STATUS;
 			}
 		};
-		job.setSystem(true);
-		job.schedule(2500);
+		startupJob.setSystem(true);
+		startupJob.schedule(2500);
+
+		Job updateJob = new Job("Checking for updates") {
+
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				ServiceReference ref = context.getServiceReference(IProvisioningAgent.class.getName());
+				IProvisioningAgent agent = (IProvisioningAgent) context.getService(ref);
+
+				IProfile profile = getProfileRegistry(agent).getProfile(IProfileRegistry.SELF);
+
+				ArrayList<IInstallableUnit> iusWithUpdates = new ArrayList<IInstallableUnit>();
+				ProvisioningContext provisioningContext = new ProvisioningContext(agent);
+				provisioningContext.setMetadataRepositories(new URI[] { URI
+						.create("http://dist.springsource.com/snapshot/TOOLS/nightly/e3.6") });
+
+				IQuery<IInstallableUnit> query = QueryUtil.createIUAnyQuery();
+				Iterator<IInstallableUnit> iter = profile.query(query, null).iterator();
+				while (iter.hasNext()) {
+					IInstallableUnit iu = iter.next();
+					IQueryResult<IInstallableUnit> replacements = getPlanner(agent).updatesFor(iu, provisioningContext,
+							null);
+					if (!replacements.isEmpty())
+						iusWithUpdates.add(iu);
+				}
+
+				context.ungetService(ref);
+				return Status.OK_STATUS;
+			}
+		};
+		updateJob.setSystem(true);
+		updateJob.schedule(500);
 	}
-	
+
+	private IProfileRegistry getProfileRegistry(IProvisioningAgent agent) {
+		return (IProfileRegistry) agent.getService(IProfileRegistry.SERVICE_NAME);
+	}
+
+	private IPlanner getPlanner(IProvisioningAgent agent) {
+		return (IPlanner) agent.getService(IPlanner.SERVICE_NAME);
+	}
+
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		for (IUsageMonitor monitor : monitors) {
