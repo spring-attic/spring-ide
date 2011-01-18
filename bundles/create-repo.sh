d8c03e011aa361ec047f3340eb9550626f918ea2
Christian Dupuis <cdupuis@vmware.com>
2011-01-18 00:40:38 +0000

    removed required-bundles and old update site project; added new bundles project for tycho build

---
+++ bundles/create-repo.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+WORKSPACE=`pwd`
+
+ECLIPSE_HOME=/Users/cdupuis/springsource-2.5.2.RELEASE-cocoa/sts-2.5.2.RELEASE
+LAUNCHER_JAR=`ls $ECLIPSE_HOME/plugins/org.eclipse.equinox.launcher_*`
+
+rm $WORKSPACE/repo/content.jar $WORKSPACE/repo/artifacts.jar
+
+$JAVA_HOME/bin/java -jar $LAUNCHER_JAR -consolelog -nosplash -verbose \
+	-application org.eclipse.equinox.p2.publisher.FeaturesAndBundlesPublisher \
+	-metadataRepository file:/$WORKSPACE/repo -artifactRepository file:/$WORKSPACE/repo \
+	-source $WORKSPACE/repo -publishArtifacts -compress -configs gtk.linux.x86
