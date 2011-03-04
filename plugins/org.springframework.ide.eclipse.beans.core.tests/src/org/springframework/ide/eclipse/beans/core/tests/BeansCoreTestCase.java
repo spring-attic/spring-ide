8a568f7a1f3233751e2821c39e433ed645a4b5c6
Martin Lippert <mlippert@vmware.com>
2011-03-04 02:54:44 +0100

    added forgotten author and copyright update

--- plugins/org.springframework.ide.eclipse.beans.core.tests/src/org/springframework/ide/eclipse/beans/core/tests/BeansCoreTestCase.java
+++ plugins/org.springframework.ide.eclipse.beans.core.tests/src/org/springframework/ide/eclipse/beans/core/tests/BeansCoreTestCase.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2008 Spring IDE Developers
+ * Copyright (c) 2005, 2011 Spring IDE Developers
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -36,6 +36,7 @@ import org.eclipse.jdt.core.JavaCore;
 
 /**
  * @author Christian Dupuis
+ * @author Martin Lippert
  * @since 2.0.3
  */
 public abstract class BeansCoreTestCase extends TestCase {
