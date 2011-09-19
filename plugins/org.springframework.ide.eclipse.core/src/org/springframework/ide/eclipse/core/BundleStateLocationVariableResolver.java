ce1467c13876dea3aeb3d680cdc10e3febca8744
Martin Lippert <mlippert@vmware.com>
2011-09-19 09:04:49 +0200

    STS-2108: fixing potential IllegalArgumentException if argument is null

--- plugins/org.springframework.ide.eclipse.core/src/org/springframework/ide/eclipse/core/BundleStateLocationVariableResolver.java
+++ plugins/org.springframework.ide.eclipse.core/src/org/springframework/ide/eclipse/core/BundleStateLocationVariableResolver.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 Spring IDE Developers
+ * Copyright (c) 2010, 2011 Spring IDE Developers
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -24,20 +24,23 @@ import org.osgi.framework.Bundle;
  * {@link IDynamicVariableResolver} that is capable to resolve <code>$
  * {bundle_state_loc}</code> to the bundle location.
  * @author Christian Dupuis
+ * @author Martin Lippert
  * @since 2.3.1
  */
 @SuppressWarnings("restriction")
 public class BundleStateLocationVariableResolver implements IDynamicVariableResolver {
 
 	public String resolveValue(IDynamicVariable variable, String argument) throws CoreException {
-		Bundle bundle = Platform.getBundle(argument);
-		if (bundle != null) {
-			IPath stateLocation = InternalPlatform.getDefault().getStateLocation(bundle, true);
-			String path = stateLocation.toString();
-			if (path != null && path.endsWith(File.separator)) {
-				return path.substring(0, path.length() - 1);
+		if (argument != null) {
+			Bundle bundle = Platform.getBundle(argument);
+			if (bundle != null) {
+				IPath stateLocation = InternalPlatform.getDefault().getStateLocation(bundle, true);
+				String path = stateLocation.toString();
+				if (path != null && path.endsWith(File.separator)) {
+					return path.substring(0, path.length() - 1);
+				}
+				return path;
 			}
-			return path;
 		}
 		return null;
 	}
