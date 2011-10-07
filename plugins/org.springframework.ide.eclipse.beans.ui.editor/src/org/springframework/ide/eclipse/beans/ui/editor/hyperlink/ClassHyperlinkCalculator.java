65e9c12b3c590ccb92fc924819889db9fc4412f1
Terry Denney <terry.denney@tasktop.com>
2011-10-07 15:28:28 -0700

    OPEN - issue STS-2136: hyperlink for class appears twice https://issuetracker.springsource.com/browse/STS-2136

--- plugins/org.springframework.ide.eclipse.beans.ui.editor/src/org/springframework/ide/eclipse/beans/ui/editor/hyperlink/ClassHyperlinkCalculator.java
+++ plugins/org.springframework.ide.eclipse.beans.ui.editor/src/org/springframework/ide/eclipse/beans/ui/editor/hyperlink/ClassHyperlinkCalculator.java
@@ -10,6 +10,9 @@
  *******************************************************************************/
 package org.springframework.ide.eclipse.beans.ui.editor.hyperlink;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jface.text.IDocument;
@@ -18,12 +21,14 @@ import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.hyperlink.IHyperlink;
 import org.springframework.ide.eclipse.beans.ui.editor.util.BeansEditorUtils;
 import org.springframework.ide.eclipse.core.java.JdtUtils;
+import org.springframework.util.ClassUtils;
 import org.w3c.dom.Node;
 
 /**
  * {@link IHyperlinkCalculator} implementation that can be used to link to a
  * {@link IType} those name is qualified in the given <code>target</code>.
  * @author Christian Dupuis
+ * @author Terry Denney
  * @since 2.0.2
  */
 public class ClassHyperlinkCalculator implements IHyperlinkCalculator {
@@ -31,11 +36,42 @@ public class ClassHyperlinkCalculator implements IHyperlinkCalculator {
 	public IHyperlink createHyperlink(String name, String target, Node node,
 			Node parentNode, IDocument document, ITextViewer textViewer,
 			IRegion hyperlinkRegion, IRegion cursor) {
+		IHyperlink[] detectedHyperlinks = getXmlJavaHyperlinks(textViewer, hyperlinkRegion);
+		if (detectedHyperlinks != null && detectedHyperlinks.length > 0) {
+			return null;
+		}
+		
 		IFile file = BeansEditorUtils.getFile(document);
 		IType type = JdtUtils.getJavaType(file.getProject(), target);
 		if (type != null) {
 			return new JavaElementHyperlink(hyperlinkRegion, type);
 		}
+		return null;
+	}
+
+	/**
+	 * Check to make sure org.eclipse.jst.jsp.ui.internal.hyperlink.XMLJavaHyperlinkDetector
+	 * does not return any hyperlinks to avoid duplicate hyperlinks shown
+	 * 
+	 * @param textViewer
+	 * @param hyperlinkRegion
+	 * @return
+	 */
+	private IHyperlink[] getXmlJavaHyperlinks(ITextViewer textViewer, IRegion hyperlinkRegion) {
+		try {
+			Class<?> clazz = Class.forName("org.eclipse.jst.jsp.ui.internal.hyperlink.XMLJavaHyperlinkDetector", false, ClassUtils.getDefaultClassLoader());
+			Object target = clazz.getConstructor().newInstance();
+			Method method = clazz.getDeclaredMethod("detectHyperlinks", ITextViewer.class, IRegion.class, boolean.class);
+			return (IHyperlink[]) method.invoke(target, textViewer, hyperlinkRegion, true);
+		} catch (ClassNotFoundException e) {
+		} catch (SecurityException e) {
+		} catch (NoSuchMethodException e) {
+		} catch (IllegalArgumentException e) {
+		} catch (IllegalAccessException e) {
+		} catch (InvocationTargetException e) {
+		} catch (InstantiationException e) {
+		}
+		
 		return null;
 	}
 }
