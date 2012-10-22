92e42e5eb0517ecc850a29ac9987f07ea54be8ab
Tomasz Zarna <tomasz.zarna@tasktop.com>
2012-10-22 14:09:39 +0200

    STS-3007: Errors in AllBeansCoreTests suite

--- plugins/org.springframework.ide.eclipse.beans.core.tests/src/org/springframework/ide/eclipse/beans/core/tests/BeansCoreTestCase.java
+++ plugins/org.springframework.ide.eclipse.beans.core.tests/src/org/springframework/ide/eclipse/beans/core/tests/BeansCoreTestCase.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2011 Spring IDE Developers
+ * Copyright (c) 2005, 2012 Spring IDE Developers
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -33,6 +33,7 @@ import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
+import org.springsource.ide.eclipse.commons.tests.util.StsTestUtil;
 
 /**
  * @author Christian Dupuis
@@ -43,17 +44,7 @@ public abstract class BeansCoreTestCase extends TestCase {
 
 	protected void tearDown() throws Exception {
 		super.tearDown();
-		IProject[] allProjects = ResourcesPlugin.getWorkspace().getRoot()
-				.getProjects();
-		for (int i = 0; i < allProjects.length; i++) {
-			IProject project = allProjects[i];
-			deleteProject(project, false);
-		}
-		allProjects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
-		for (int i = 0; i < allProjects.length; i++) {
-			IProject project = allProjects[i];
-			deleteProject(project, true);
-		}
+		StsTestUtil.deleteAllProjects();
 	}
 
 	/**
@@ -248,79 +239,6 @@ public abstract class BeansCoreTestCase extends TestCase {
 
 	protected IProject getProject(String project) {
 		return getWorkspaceRoot().getProject(project);
-	}
-
-	protected void deleteProject(IProject project, boolean force)
-			throws CoreException {
-		if (project.exists() && !project.isOpen()) { // force opening so that
-														// project can be
-														// deleted without
-														// logging (see bug
-														// 23629)
-			project.open(null);
-		}
-		deleteResource(project, force);
-	}
-
-	protected void deleteProject(String projectName) throws CoreException {
-		deleteProject(this.getProject(projectName), true);
-	}
-
-	/**
-	 * Delete this resource.
-	 */
-	public void deleteResource(IResource resource, boolean force)
-			throws CoreException {
-		waitForManualBuild();
-		waitForAutoBuild();
-		CoreException lastException = null;
-		try {
-			resource.delete(false, null);
-		}
-		catch (CoreException e) {
-			lastException = e;
-			// just print for info
-			System.out
-					.println("(CoreException): " + e.getMessage() + " Resource " + resource.getFullPath()); //$NON-NLS-1$ //$NON-NLS-2$
-			e.printStackTrace();
-		}
-		catch (IllegalArgumentException iae) {
-			// just print for info
-			System.out
-					.println("(IllegalArgumentException): " + iae.getMessage() + ", resource " + resource.getFullPath()); //$NON-NLS-1$ //$NON-NLS-2$
-		}
-		if (!force) {
-			return;
-		}
-		int retryCount = 10; // wait 1 minute at most
-		while (resource.isAccessible() && --retryCount >= 0) {
-			waitForAutoBuild();
-			try {
-				Thread.sleep(1000);
-			}
-			catch (InterruptedException e) {
-			}
-			try {
-				resource.delete(true, null);
-			}
-			catch (CoreException e) {
-				lastException = e;
-				// just print for info
-				System.out
-						.println("(CoreException) Retry " + retryCount + ": " + e.getMessage() + ", resource " + resource.getFullPath()); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-			}
-			catch (IllegalArgumentException iae) {
-				// just print for info
-				System.out
-						.println("(IllegalArgumentException) Retry " + retryCount + ": " + iae.getMessage() + ", resource " + resource.getFullPath()); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-			}
-		}
-		if (!resource.isAccessible())
-			return;
-		System.err.println("Failed to delete " + resource.getFullPath()); //$NON-NLS-1$
-		if (lastException != null) {
-			throw lastException;
-		}
 	}
 
 	protected IResource createPredefinedProjectAndGetResource(
