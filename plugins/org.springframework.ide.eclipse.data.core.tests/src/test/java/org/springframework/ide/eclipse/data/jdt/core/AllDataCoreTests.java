fba0a3e441bdece13584e361686b5c2511776170
Tomasz Zarna <tomasz.zarna@tasktop.com>
2012-12-20 12:58:32 +0100

    STS-3039: Tests from o.s.i.e.data.core.tests are not part of any suite

---
+++ plugins/org.springframework.ide.eclipse.data.core.tests/src/test/java/org/springframework/ide/eclipse/data/jdt/core/AllDataCoreTests.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Spring IDE Developers
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Spring IDE Developers - initial API and implementation
+ *******************************************************************************/
+package org.springframework.ide.eclipse.data.jdt.core;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author Tomasz Zarna
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ KeywordProviderSupportUnitTests.class, //
+		QueryMethodCandidateUnitTests.class, //
+		QueryMethodPartUnitTests.class //
+})
+public class AllDataCoreTests {
+	// goofy junit4, no class body needed
+}
