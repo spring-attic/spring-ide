787709fda3db6b1959d3c9e9e256886bd31a9ef2
Leo Dos Santos <leo.dos.santos@tasktop.com>
2013-02-01 16:13:19 -0800

    STS-3186: reflective call for differences between M2E 1.0 and 1.1

--- plugins/org.springframework.ide.eclipse.roo.ui/src/org/springframework/ide/eclipse/roo/ui/internal/maven/RooProjectConfigurator.java
+++ plugins/org.springframework.ide.eclipse.roo.ui/src/org/springframework/ide/eclipse/roo/ui/internal/maven/RooProjectConfigurator.java
@@ -10,20 +10,26 @@
  *******************************************************************************/
 package org.springframework.ide.eclipse.roo.ui.internal.maven;
 
+import java.lang.reflect.Constructor;
+
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.project.MavenProject;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.m2e.core.MavenPlugin;
 import org.eclipse.m2e.core.internal.lifecyclemapping.LifecycleMappingFactory;
 import org.eclipse.m2e.core.internal.project.LifecycleMappingConfiguration;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
 import org.eclipse.m2e.core.project.MavenProjectChangedEvent;
 import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
-import org.eclipse.m2e.core.ui.internal.UpdateMavenProjectJob;
 import org.springframework.ide.eclipse.core.SpringCore;
 import org.springframework.ide.eclipse.maven.AbstractSpringProjectConfigurator;
 import org.springframework.ide.eclipse.roo.core.RooCoreActivator;
+import org.springframework.ide.eclipse.roo.ui.RooUiActivator;
 import org.springframework.ide.eclipse.roo.ui.internal.actions.OpenShellJob;
 import org.springsource.ide.eclipse.commons.core.SpringCoreUtils;
 
@@ -81,8 +87,39 @@ public class RooProjectConfigurator extends AbstractSpringProjectConfigurator {
 			LifecycleMappingConfiguration oldConfiguration = LifecycleMappingConfiguration.restore(facade, monitor);
 			if (oldConfiguration != null
 					&& LifecycleMappingFactory.isLifecycleMappingChanged(facade, oldConfiguration, monitor)) {
-				new UpdateMavenProjectJob(new IProject[] { facade.getProject() }).schedule();
+				updateProjectConfiguration(facade.getProject());
 			}
+		}
+	}
+	
+	private void updateProjectConfiguration(IProject project) {
+		// TODO: remove this reflection if we ever set a minimum requirement of M2E 1.1
+		try {
+			Class<?> jobClass;
+			Constructor<?> constr;
+			WorkspaceJob job = null;
+			try {
+				// M2E 1.1 and greater
+				jobClass = Class.forName("org.eclipse.m2e.core.ui.internal.UpdateMavenProjectJob");
+				constr = jobClass.getConstructor(IProject[].class, boolean.class, boolean.class,
+						boolean.class, boolean.class);
+				job = (WorkspaceJob) constr.newInstance(new IProject[] { project }, 
+						MavenPlugin.getMavenConfiguration().isOffline(), false, true, true);
+
+			} catch (ClassNotFoundException e) {
+				// M2E 1.0
+				jobClass = Class.forName("org.eclipse.m2e.core.ui.internal.UpdateConfigurationJob");
+				constr = jobClass.getConstructor(IProject[].class, boolean.class, boolean.class);
+				job = (WorkspaceJob) constr.newInstance(
+						new IProject[] { project }, MavenPlugin.getMavenConfiguration().isOffline(), false);
+			}
+			if (job != null) {
+				job.schedule();
+			}
+		}
+		catch (Throwable e) {
+			RooCoreActivator.log(new Status(IStatus.ERROR, RooUiActivator.PLUGIN_ID,
+					"Unable to update Maven project configuration", e));
 		}
 	}
 
