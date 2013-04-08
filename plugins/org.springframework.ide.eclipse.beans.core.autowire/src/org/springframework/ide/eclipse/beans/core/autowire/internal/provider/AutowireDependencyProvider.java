82d8fb5a6a02cef3678e7416ed99bc55905f49c4
Martin Lippert <mlippert@vmware.com>
2013-04-08 18:56:52 +0200

    STS-3245: deleted old deprecated classes and switched included libs to use bundle dependencies instead

--- plugins/org.springframework.ide.eclipse.beans.core.autowire/src/org/springframework/ide/eclipse/beans/core/autowire/internal/provider/AutowireDependencyProvider.java
+++ plugins/org.springframework.ide.eclipse.beans.core.autowire/src/org/springframework/ide/eclipse/beans/core/autowire/internal/provider/AutowireDependencyProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2012 Spring IDE Developers
+ * Copyright (c) 2009, 2013 Spring IDE Developers
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -25,8 +25,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-import javax.annotation.Resource;
-import javax.ejb.EJB;
 import javax.inject.Provider;
 
 import org.eclipse.core.resources.IMarker;
