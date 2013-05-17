2dd0d5491c9c4fe564aa01a8e38d1dfcad91904d
nierajsingh <nierajsingh@vmware.com>
2013-05-16 19:19:52 -0700

    Added Spring Version and Spring Version processor to replace version in Maven POM.

--- plugins/org.springframework.ide.eclipse.wizard/src/org/springframework/ide/eclipse/wizard/template/SpringVersion.java
+++ plugins/org.springframework.ide.eclipse.wizard/src/org/springframework/ide/eclipse/wizard/template/SpringVersion.java
@@ -13,24 +13,89 @@ package org.springframework.ide.eclipse.wizard.template;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a released Spring version. A Default value also exists which
+ * indicates that the default spring version as defined in a template should be
+ * used.
+ * 
+ */
 public class SpringVersion {
 
-	private final double version;
+	private final String fullVersion;
 
-	public SpringVersion(double version) {
-		this.version = version;
+	private final String display;
+
+	public static final SpringVersion DEFAULT = new SpringVersion("Default");
+
+	public SpringVersion(String display) {
+		this.display = display;
+		this.fullVersion = display + ".RELEASE";
 	}
 
-	public double getVersion() {
-		return version;
+	public String getDisplay() {
+		return display;
 	}
 
+	public String getVersion() {
+		return fullVersion;
+	}
+
+	/**
+	 * Returns RELEASE versions of Spring only.
+	 * @return
+	 */
 	public static List<SpringVersion> getVersions() {
 		List<SpringVersion> versions = new ArrayList<SpringVersion>();
+		// TODO: Hardcoded for now. Externalise, or find a way to read supported
+		// release versions
+		versions.add(DEFAULT);
+		versions.add(new SpringVersion("3.2.2"));
+		versions.add(new SpringVersion("3.1.4"));
+		versions.add(new SpringVersion("3.1.1"));
+		versions.add(new SpringVersion("3.0.7"));
+		versions.add(new SpringVersion("2.5.6"));
 
-		versions.add(new SpringVersion(3.1));
-		versions.add(new SpringVersion(4.0));
 		return versions;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((display == null) ? 0 : display.hashCode());
+		result = prime * result + ((fullVersion == null) ? 0 : fullVersion.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		SpringVersion other = (SpringVersion) obj;
+		if (display == null) {
+			if (other.display != null) {
+				return false;
+			}
+		}
+		else if (!display.equals(other.display)) {
+			return false;
+		}
+		if (fullVersion == null) {
+			if (other.fullVersion != null) {
+				return false;
+			}
+		}
+		else if (!fullVersion.equals(other.fullVersion)) {
+			return false;
+		}
+		return true;
 	}
 
 }
