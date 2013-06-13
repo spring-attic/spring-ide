bdd67f91f899b90a47bc7e1afbe75118b170eec7
Martin Lippert <mlippert@gopivotal.com>
2013-06-13 12:30:32 +0200

    STS-3407: added test cases in preparation of changing the BeansModelUtils methods for finding affected beans and configs from IResource objects

---
+++ plugins/org.springframework.ide.eclipse.beans.core.tests/workspace/beans-model-utils-tests/src/org/test/spring/FactoryMethodBean.java
@@ -0,0 +1,9 @@
+package org.test.spring;
+
+public class FactoryMethodBean {
+	
+	public FactoryCreatedBean createBean() {
+		return null;
+	}
+
+}
---
+++ plugins/org.springframework.ide.eclipse.beans.core.tests/workspace/beans-model-utils-tests/src/org/test/spring/SupertypeFactoryMethodBean.java
@@ -0,0 +1,9 @@
+package org.test.spring;
+
+public class SupertypeFactoryMethodBean {
+
+	public SupertypeMethodCreatedBean createSupertypeBean() {
+		return null;
+	}
+
+}
