dbad8e2b579147496b03f6bc168f49deabfec0e3
Leo Dos Santos <leo.dos.santos@tasktop.com>
2013-06-28 16:01:16 -0700

    STS-3448: The Spring Tools menu appears when it shouldn't in common navigator views in Eclipse 3.8 https://issuetracker.springsource.com/browse/STS-3448

--- plugins/org.springframework.ide.eclipse.beans.core/src/org/springframework/ide/eclipse/beans/core/internal/model/BeansModelPropertyTester.java
+++ plugins/org.springframework.ide.eclipse.beans.core/src/org/springframework/ide/eclipse/beans/core/internal/model/BeansModelPropertyTester.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2009 Spring IDE Developers
+ * Copyright (c) 2006 - 2013 Spring IDE Developers
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,11 +10,16 @@
  *******************************************************************************/
 package org.springframework.ide.eclipse.beans.core.internal.model;
 
+import java.util.Set;
+
 import org.eclipse.core.expressions.PropertyTester;
 import org.eclipse.core.resources.IFile;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.ide.eclipse.beans.core.BeansCoreUtils;
 import org.springframework.ide.eclipse.beans.core.model.IBean;
+import org.springframework.ide.eclipse.beans.core.model.IBeansConfig;
+import org.springframework.ide.eclipse.beans.core.model.IBeansConfigSet;
+import org.springframework.ide.eclipse.beans.core.model.IBeansProject;
 
 /**
  * This {@link PropertyTester} is used to check properties of the BeansCoreModel in <code><test property="..."/></code>
@@ -28,6 +33,7 @@ import org.springframework.ide.eclipse.beans.core.model.IBean;
  * </ul>
  * @author Torsten Juergeleit
  * @author Christian Dupuis
+ * @author Leo Dos Santos
  */
 public class BeansModelPropertyTester extends PropertyTester {
 
@@ -35,12 +41,31 @@ public class BeansModelPropertyTester extends PropertyTester {
 		if (receiver instanceof IFile && "isBeansConfig".equals(property)) {
 			boolean isBeansConfig = BeansCoreUtils.isBeansConfig((IFile) receiver, true);
 			return expectedValue == null ? isBeansConfig : isBeansConfig == ((Boolean) expectedValue).booleanValue();
-		}
-		else if (receiver instanceof IBean && "isInfrstructureBean".equals(property)) {
+		} else if (receiver instanceof IBean && "isInfrstructureBean".equals(property)) {
 			boolean isInfrstructureBean = ((IBean) receiver).isInfrastructure();
 			return expectedValue == null ? isInfrstructureBean : isInfrstructureBean == ((Boolean) expectedValue)
 					.booleanValue();
+		} else if (receiver instanceof IBeansConfig && "canAddToConfigSet".equals(property)) {
+			 IBeansConfig config = (IBeansConfig) receiver;
+			 boolean canAddToConfigSet = hasAvailableConfigSet(config);
+			 return expectedValue == null ? canAddToConfigSet : canAddToConfigSet == ((Boolean) expectedValue);
+		} else if (receiver instanceof IBeansConfigSet && "isEmptyConfigSet".equals(property)) {
+			IBeansConfigSet configSet = (IBeansConfigSet) receiver;
+			boolean isEmpty = configSet.getConfigs().isEmpty();
+			return expectedValue == null ? isEmpty : isEmpty == ((Boolean) expectedValue);
 		}
 		return false;
 	}
+	
+	private boolean hasAvailableConfigSet(IBeansConfig config) {
+		IBeansProject project = BeansModelUtils.getProject(config);
+		Set<IBeansConfigSet> configSets = project.getConfigSets();
+		for (IBeansConfigSet configSet : configSets) {
+			if (!configSet.hasConfig(config.getElementName())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 }
