0141c92982f5ab6800135d6fd9d32b4081a7a87c
Kris De Volder <kdevolder@gopivotal.com>
2013-09-05 17:12:05 -0700

    STS-3545: SWTException thrown when closing import guides wizard while downloading

--- plugins/org.springframework.ide.eclipse.wizard/src/org/springframework/ide/eclipse/wizard/gettingstarted/guides/CodeSetCheckBoxesSection.java
+++ plugins/org.springframework.ide.eclipse.wizard/src/org/springframework/ide/eclipse/wizard/gettingstarted/guides/CodeSetCheckBoxesSection.java
@@ -126,6 +126,11 @@ public class CodeSetCheckBoxesSection extends WizardPageSection {
 		options.addListener(new UIValueListener<String[]>() {
 			@Override
 			public void uiGotValue(LiveExpression<String[]> exp, String[] names) {
+				if (group==null || !group.isDisposed()) {
+					//Don't bother. The UI is already gone.
+					options.removeListener(this);
+					return;
+				}
 				if (names==null) {
 					names = new String[0];
 				}
