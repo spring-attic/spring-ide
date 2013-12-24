928ca65431510a5767ad69b02e7617bab2021a08
Kris De Volder <kdevolder@gopivotal.com>
2013-12-24 12:34:54 -0800

    Meaningless edit to kick build server into action.

--- plugins/org.springframework.ide.eclipse.wizard/src/org/springframework/ide/eclipse/wizard/gettingstarted/guides/GSImportWizard.java
+++ plugins/org.springframework.ide.eclipse.wizard/src/org/springframework/ide/eclipse/wizard/gettingstarted/guides/GSImportWizard.java
@@ -6,7 +6,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    GoPivotal, Inc. - initial API and implementation
+ *   GoPivotal, Inc. - initial API and implementation
  *******************************************************************************/
 package org.springframework.ide.eclipse.wizard.gettingstarted.guides;
 
