33885d6ca8954088072203bba831af970cc5112a
Martin Lippert <mlippert@gopivotal.com>
2014-11-10 17:25:44 +0100

    fix missing java-config-aware label provider

--- plugins/org.springframework.ide.eclipse.webflow.ui/src/org/springframework/ide/eclipse/webflow/ui/properties/BeansConfigLabelProvider.java
+++ plugins/org.springframework.ide.eclipse.webflow.ui/src/org/springframework/ide/eclipse/webflow/ui/properties/BeansConfigLabelProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 Spring IDE Developers
+ * Copyright (c) 2007, 2014 Spring IDE Developers
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,7 +10,9 @@
  *******************************************************************************/
 package org.springframework.ide.eclipse.webflow.ui.properties;
 
+import org.eclipse.jdt.core.IType;
 import org.eclipse.jface.viewers.ILabelProvider;
+import org.springframework.ide.eclipse.beans.core.internal.model.BeansJavaConfig;
 import org.springframework.ide.eclipse.beans.core.model.IBeansConfig;
 import org.springframework.ide.eclipse.beans.ui.model.BeansModelLabelProvider;
 
@@ -29,7 +31,20 @@ public class BeansConfigLabelProvider extends BeansModelLabelProvider {
 	@Override
 	protected String getText(Object element, Object parentElement) {
 		if (element instanceof IBeansConfig) {
-			return ((IBeansConfig) element).getElementName();
+			String text = ((IBeansConfig) element).getElementName();
+			if (text.startsWith(IBeansConfig.EXTERNAL_FILE_NAME_PREFIX)) {
+				text = text.substring(IBeansConfig.EXTERNAL_FILE_NAME_PREFIX.length());
+			}
+			if (element instanceof BeansJavaConfig) {
+				IType configClass = ((BeansJavaConfig) element).getConfigClass();
+				if (configClass != null) {
+					text = ((BeansJavaConfig) element).getConfigClass().getElementName();
+				}
+				else {
+					text = ((BeansJavaConfig) element).getConfigClassName();
+				}
+			}
+			return text;
 		}
 		return super.getText(element, parentElement);
 	}
