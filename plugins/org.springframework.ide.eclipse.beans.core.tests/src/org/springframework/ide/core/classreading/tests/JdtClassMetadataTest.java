91e4b83e575632594acecf406e91d50c87469900
Martin Lippert <mlippert@pivotal.io>
2015-01-07 10:57:59 +0100

    fixing failing tests due to update to Spring framework 4.1.4

--- plugins/org.springframework.ide.eclipse.beans.core.tests/src/org/springframework/ide/core/classreading/tests/JdtClassMetadataTest.java
+++ plugins/org.springframework.ide.eclipse.beans.core.tests/src/org/springframework/ide/core/classreading/tests/JdtClassMetadataTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2014 Spring IDE Developers
+ * Copyright (c) 2012, 2015 Spring IDE Developers
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -143,8 +143,7 @@ public class JdtClassMetadataTest {
 		assertFalse(metadata.hasEnclosingClass());
 		assertNull(metadata.getEnclosingClassName());
 
-		assertTrue(metadata.hasSuperClass());
-		assertEquals("java.lang.Object", metadata.getSuperClassName());
+		assertFalse(metadata.hasSuperClass());
 	}
 
 	@Test
@@ -159,8 +158,7 @@ public class JdtClassMetadataTest {
 		assertFalse(metadata.hasEnclosingClass());
 		assertNull(metadata.getEnclosingClassName());
 
-		assertTrue(metadata.hasSuperClass());
-		assertEquals("java.lang.Object", metadata.getSuperClassName());
+		assertFalse(metadata.hasSuperClass());
 	}
 
 }
