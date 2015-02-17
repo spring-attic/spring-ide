b892ed990ca311921653fb75821380837d991612
Kris De Volder <kdevolder@pivotal.io>
2015-02-17 11:45:02 -0800

    Add intializr v2.1 API sample json to test resources (for later use)

--- plugins/org.springframework.ide.eclipse.boot.test/src/org/springframework/ide/eclipse/boot/test/pretty.js
+++ plugins/org.springframework.ide.eclipse.boot.test/src/org/springframework/ide/eclipse/boot/test/pretty.js
@@ -1,5 +1,5 @@
-    /*global require console*/
-    var fs = require('fs');
-    var file = fs.readFileSync("initializr.json", "utf8");
-    var obj = JSON.parse(file);
-    console.log(JSON.stringify(obj, null, "   "));
+/*global require console*/
+var fs = require('fs');
+var file = fs.readFileSync("initializr-v2.1.json", "utf8");
+var obj = JSON.parse(file);
+console.log(JSON.stringify(obj, null, "   "));
