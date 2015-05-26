fc923d4de85464373cadb91798fae0a72dd535a5
Kris De Volder <kdevolder@pivotal.io>
2015-05-26 13:34:04 -0700

    'destroyForcibly' is Java 8 only api. Call via reflection if available.

--- plugins/org.springframework.ide.eclipse.boot.launch/src/org/springframework/ide/eclipse/boot/launch/process/BootProcessFactory.java
+++ plugins/org.springframework.ide.eclipse.boot.launch/src/org/springframework/ide/eclipse/boot/launch/process/BootProcessFactory.java
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package org.springframework.ide.eclipse.boot.launch.process;
 
+import java.lang.reflect.Method;
 import java.util.Map;
 
 import org.eclipse.debug.core.DebugException;
@@ -20,33 +21,56 @@ import org.eclipse.debug.core.model.RuntimeProcess;
 
 public class BootProcessFactory implements IProcessFactory {
 
+	/**
+	 * Method only available on Java 8.
+	 */
+	public static Method destroyForcibly;
+	static {
+		try {
+			destroyForcibly = Process.class.getMethod("destroyForcibly");
+		} catch (Exception e) {
+			destroyForcibly = null;
+		}
+	}
+
 	@Override
 	public IProcess newProcess(ILaunch launch, Process process, String label,
 			Map<String, String> attributes) {
-		return new RuntimeProcess(launch, process, label, attributes) {
-			@Override
-			public void terminate() throws DebugException {
-				Process sysproc = getSystemProcess();
-				if (sysproc!=null) {
-					//Superclass 'terminate' method is unreliable so...
-					try {
-						super.terminate();
-					} catch (DebugException de) {
-						//Failed so be more aggressive
-						sysproc.destroyForcibly();
-						boolean done = false;
-						while (!done) {
+
+		if (destroyForcibly!=null) {
+			return new RuntimeProcess(launch, process, label, attributes) {
+				@Override
+				public void terminate() throws DebugException {
+					Process sysproc = getSystemProcess();
+					if (sysproc!=null) {
+						//Superclass 'terminate' method is unreliable so...
+						try {
+							super.terminate();
+						} catch (DebugException de) {
 							try {
-								sysproc.waitFor();
-								done = true;
-							} catch (InterruptedException ie) {
-								//ignore
+								//Failed so be more aggressive
+								destroyForcibly.invoke(sysproc);
+								boolean done = false;
+								while (!done) {
+									try {
+										sysproc.waitFor();
+										done = true;
+									} catch (InterruptedException ie) {
+										//ignore
+									}
+								}
+							} catch (Exception e) {
+								//Something unexpected went wrong trying to call 'sysproc.destroyForcibly' or 'sysproc.waitFor'
+								throw de;
 							}
 						}
 					}
 				}
-			}
-		};
+			};
+		} else {
+			//Not Java 8 so can't use the destroyForcibly to force faster process termination,
+			return new RuntimeProcess(launch, process, label, attributes);
+		}
 	}
 
 }
