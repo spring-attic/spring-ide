2d74326bb218e16652ba2ad9ad76585f333a995b
Kris De Volder <kdevolder@pivotal.io>
2015-05-29 10:59:34 -0700

    Create a test plugin for boot.dash, add one dummy test

---
+++ plugins/org.springframework.ide.eclipse.boot.dash.test/src/org/springframework/ide/eclipse/boot/dash/test/AllBootDashTests.java
@@ -0,0 +1,14 @@
+package org.springframework.ide.eclipse.boot.dash.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	BootDashModelTest.class
+})
+public class AllBootDashTests {
+
+}
---
+++ plugins/org.springframework.ide.eclipse.boot.dash.test/src/org/springframework/ide/eclipse/boot/dash/test/BootDashModelTest.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Pivotal Software, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Pivotal Software, Inc. - initial API and implementation
+ *******************************************************************************/
+package org.springframework.ide.eclipse.boot.dash.test;
+
+/**
+ * @author Kris De Volder
+ */
+public class BootDashModelTest extends BootDashModelTestHarness {
+
+	public void testDummy() throws Exception {
+		fail("This test should fail!");
+	}
+
+}
