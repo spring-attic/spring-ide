261e75fc846ec2902ddf51640b3a49435a0f429b
nsingh <nsingh@pivotal.io>
2015-09-10 21:47:49 -0700

    Further fixes for stop button support for CF apps.

--- plugins/org.springframework.ide.eclipse.boot.dash/src/org/springframework/ide/eclipse/boot/dash/cloudfoundry/ops/DeploymentOperationFactory.java
+++ plugins/org.springframework.ide.eclipse.boot.dash/src/org/springframework/ide/eclipse/boot/dash/cloudfoundry/ops/DeploymentOperationFactory.java
@@ -17,60 +17,69 @@ import org.cloudfoundry.client.lib.domain.CloudDomain;
 import org.eclipse.core.resources.IProject;
 import org.springframework.ide.eclipse.boot.dash.cloudfoundry.CloudApplicationDeploymentProperties;
 import org.springframework.ide.eclipse.boot.dash.cloudfoundry.CloudFoundryBootDashModel;
+import org.springframework.ide.eclipse.boot.dash.model.RunState;
+import org.springframework.ide.eclipse.boot.dash.model.UserInteractions;
 
 public class DeploymentOperationFactory {
 
 	private final CloudFoundryBootDashModel model;
 	private final CloudApplicationDeploymentProperties properties;
-	private final List<CloudDomain> domains;
 	private final IProject project;
+	private final UserInteractions ui;
 
 	public DeploymentOperationFactory(CloudFoundryBootDashModel model, CloudApplicationDeploymentProperties properties,
-			IProject project, List<CloudDomain> domains) {
+			IProject project, List<CloudDomain> domains, UserInteractions ui) {
 		this.model = model;
 		this.project = project;
 		this.properties = properties;
-		this.domains = domains;
+		this.ui = ui;
 	}
 
 	public CloudApplicationOperation getDeploymentOperationExistingApp() {
-		List<CloudApplicationOperation> uploadAndRestartOps = uploadUpdateRestartOps();
+		List<CloudApplicationOperation> uploadAndRestartOps = getUploadUpdateRestartOps(RunState.STARTING);
 
 		CloudApplicationOperation op = new CompositeApplicationOperation(
 				"Re-deploying and re-starting project: " + project.getName(), model, properties.getAppName(),
-				uploadAndRestartOps, true);
-		op.addApplicationUpdateListener(new FullAppDeploymentListener(properties.getAppName(), model));
+				uploadAndRestartOps);
 		return op;
 	}
 
 	public CloudApplicationOperation getDeploymentOperation() {
 		List<CloudApplicationOperation> deploymentOperations = new ArrayList<CloudApplicationOperation>();
 
-		CloudApplicationOperation createOp = new ApplicationCreateOperation(properties, model);
+		CloudApplicationOperation createOp = new ApplicationCreateOperation(properties, model, RunState.STARTING);
 		deploymentOperations.add(createOp);
 
-		List<CloudApplicationOperation> uploadAndRestartOps = uploadUpdateRestartOps();
+		List<CloudApplicationOperation> uploadAndRestartOps = getUploadUpdateRestartOps(null);
 		deploymentOperations.addAll(uploadAndRestartOps);
 
 		CloudApplicationOperation op = new CompositeApplicationOperation(
 				"Deploying and starting project: " + project.getName(), model, properties.getAppName(),
-				deploymentOperations, true);
-		op.addApplicationUpdateListener(new FullAppDeploymentListener(properties.getAppName(), model));
+				deploymentOperations);
 		return op;
 	}
 
-	protected List<CloudApplicationOperation> uploadUpdateRestartOps() {
+	/**
+	 *
+	 * @param preferredRunState
+	 *            Optional: state at which the application should be when the
+	 *            upload and restart operation is run. If null, the existing
+	 *            runstate of the app will be retained.
+	 * @return non-null list of ops that perform upload, update and restart
+	 */
+	protected List<CloudApplicationOperation> getUploadUpdateRestartOps(RunState preferredRunState) {
 		List<CloudApplicationOperation> deploymentOperations = new ArrayList<CloudApplicationOperation>();
-		CloudApplicationOperation uploadOp = new ApplicationUploadOperation(properties, model);
-		deploymentOperations.add(uploadOp);
+		// set the preferred runstate in the first op that gets executed. Not
+		// necessary to add it to any of the following ops
+		// as the app will remain in the preferred run state unless any one of
+		// the ops, or any other op running in parallel, changes the state to
+		// something else
+		deploymentOperations.add(new ApplicationUploadOperation(properties, model, ui, preferredRunState));
 
-		CloudApplicationOperation updateOp = new ApplicationPropertiesUpdateOperation(properties, model);
-		deploymentOperations.add(updateOp);
+		deploymentOperations.add(new ApplicationPropertiesUpdateOperation(properties, model));
 
-		CloudApplicationOperation restartOp = new ApplicationStartOperation(properties.getAppName(), model);
-		deploymentOperations.add(restartOp);
+		deploymentOperations.add(new ApplicationStartOperation(properties.getAppName(), model));
 
 		return deploymentOperations;
 	}
-
 }
