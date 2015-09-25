c13f2061f3b8d9bc7e869685c879182ce1ac9e18
Kris De Volder <kdevolder@pivotal.io>
2015-09-25 16:15:46 -0700

    Add an 'error' state to 'RefreshState' (not yet used).

--- plugins/org.springframework.ide.eclipse.boot.dash/src/org/springframework/ide/eclipse/boot/dash/model/RefreshState.java
+++ plugins/org.springframework.ide.eclipse.boot.dash/src/org/springframework/ide/eclipse/boot/dash/model/RefreshState.java
@@ -16,7 +16,81 @@ package org.springframework.ide.eclipse.boot.dash.model;
  * @author Alex Boyko
  * @author Kris De Volder
  */
-public enum RefreshState {
-	READY,
-	LOADING
+public abstract class RefreshState {
+	public static final RefreshState READY = new SimpleState("READY");
+	public static final RefreshState LOADING = new SimpleState("LOADING");
+	public static RefreshState error(String msg) {
+		return new ErrorState(msg);
+	}
+	public abstract boolean isError();
+
+	////////////////////////// implementation //////////////////////////////////////
+
+	private static class SimpleState extends RefreshState {
+
+		private String id;
+
+		public SimpleState(String id) {
+			this.id = id;
+		}
+
+		@Override
+		public boolean isError() {
+			return false;
+		}
+
+		@Override
+		public String toString() {
+			return id;
+		}
+	}
+
+	private static class ErrorState extends RefreshState {
+
+		private final String msg;
+
+		public ErrorState(String msg) {
+			this.msg = msg;
+		}
+
+		@Override
+		public String toString() {
+			if (msg!=null) {
+				return "ERROR: "+msg;
+			} else {
+				return "ERROR";
+			}
+		}
+
+		@Override
+		public boolean isError() {
+			return true;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((msg == null) ? 0 : msg.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			ErrorState other = (ErrorState) obj;
+			if (msg == null) {
+				if (other.msg != null)
+					return false;
+			} else if (!msg.equals(other.msg))
+				return false;
+			return true;
+		}
+
+	}
 }
