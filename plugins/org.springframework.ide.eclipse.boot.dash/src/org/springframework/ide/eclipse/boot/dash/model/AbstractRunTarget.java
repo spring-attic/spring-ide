16e2ae27eacd145afb28d3c6460281279f5968e6
Kris De Volder <kdevolder@pivotal.io>
2015-10-14 15:53:27 -0700

    Fix hashcode and equals in AbstractRunTarget
    Should take type into account

--- plugins/org.springframework.ide.eclipse.boot.dash/src/org/springframework/ide/eclipse/boot/dash/model/AbstractRunTarget.java
+++ plugins/org.springframework.ide.eclipse.boot.dash/src/org/springframework/ide/eclipse/boot/dash/model/AbstractRunTarget.java
@@ -49,6 +49,7 @@ public abstract class AbstractRunTarget implements RunTarget {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
 		return result;
 	}
 
@@ -65,6 +66,11 @@ public abstract class AbstractRunTarget implements RunTarget {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
 			return false;
 		return true;
 	}
