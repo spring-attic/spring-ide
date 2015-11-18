8a1d9c8aa56008325ce46fc875b2591af2edd642
Kris De Volder <kdevolder@pivotal.io>
2015-11-18 12:22:18 -0800

    Add a new 'general' boot prefs page.
    This page is currently still empty. Other pages have been 
    reorganized to make the hierarchy of boot-related prefs pages 
    more logical.

---
+++ plugins/org.springframework.ide.eclipse.boot/src/org/springframework/ide/eclipse/boot/ui/preferences/BootPreferencePage.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ *  Copyright (c) 2015 Pivotal, Inc.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *      Pivotal, Inc. - initial API and implementation
+ *******************************************************************************/
+package org.springframework.ide.eclipse.boot.ui.preferences;
+
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+/**
+ * @author Kris De Volder
+ */
+public class BootPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+
+	@Override
+	public void init(IWorkbench workbench) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void createFieldEditors() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
