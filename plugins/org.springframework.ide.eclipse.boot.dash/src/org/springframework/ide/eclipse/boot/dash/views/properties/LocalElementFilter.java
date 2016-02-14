3d3575dcbca8ca6b7a99ef0be3c6f3d1d96e3f26
BoykoAlex <aboyko@pivotal.io>
2016-02-14 11:14:37 -0500

    Request Mappings props section available for local config dash elements

--- plugins/org.springframework.ide.eclipse.boot.dash/src/org/springframework/ide/eclipse/boot/dash/views/properties/LocalElementFilter.java
+++ plugins/org.springframework.ide.eclipse.boot.dash/src/org/springframework/ide/eclipse/boot/dash/views/properties/LocalElementFilter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Pivotal, Inc.
+ * Copyright (c) 2015, 2016 Pivotal, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,7 @@ package org.springframework.ide.eclipse.boot.dash.views.properties;
 
 import org.eclipse.jface.viewers.IFilter;
 import org.springframework.ide.eclipse.boot.dash.model.BootDashElement;
-import org.springframework.ide.eclipse.boot.dash.model.LocalRunTarget;
+import org.springframework.ide.eclipse.boot.dash.model.runtargettypes.RunTargetTypes;
 
 /**
  * Filter for local app elements for the properties section
@@ -24,7 +24,7 @@ public class LocalElementFilter implements IFilter {
 
 	@Override
 	public boolean select(Object toTest) {
-		return toTest instanceof BootDashElement && ((BootDashElement)toTest).getTarget() instanceof LocalRunTarget;
+		return toTest instanceof BootDashElement && RunTargetTypes.LOCAL.equals(((BootDashElement)toTest).getTarget().getType());
 	}
 
 }
