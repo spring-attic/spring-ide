c61e405454210954ac9605b830081e3824aed1a6
Kris De Volder <kdevolder@pivotal.io>
2016-03-16 12:31:15 -0700

    Add missing copyright header

--- plugins/org.springframework.ide.eclipse.boot.dash.test/src/org/springframework/ide/eclipse/boot/dash/test/mocks/CancelationTokens.java
+++ plugins/org.springframework.ide.eclipse.boot.dash.test/src/org/springframework/ide/eclipse/boot/dash/test/mocks/CancelationTokens.java
@@ -1,10 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2015, 2016 Pivotal, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Pivotal, Inc. - initial API and implementation
+ *******************************************************************************/
 package org.springframework.ide.eclipse.boot.dash.test.mocks;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import org.eclipse.core.runtime.Assert;
-import org.springframework.ide.eclipse.boot.dash.test.mocks.CancelationTokens.CancelationToken;
 
 /**
  * Manages a set of CancelationTokens.
