9fe4cfac78d07229b9ace4dd9ce47694c5180eb4
Kris De Volder <kdevolder@pivotal.io>
2016-03-17 09:59:44 -0700

    Cleanup imports

--- plugins/org.springframework.ide.eclipse.boot.dash.test/src/org/springframework/ide/eclipse/boot/dash/test/CloudFoundryBootDashModelMockingTest.java
+++ plugins/org.springframework.ide.eclipse.boot.dash.test/src/org/springframework/ide/eclipse/boot/dash/test/CloudFoundryBootDashModelMockingTest.java
@@ -19,10 +19,11 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.same;
 import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
 import static org.springframework.ide.eclipse.boot.dash.test.BootDashModelTest.waitForJobsToComplete;
 import static org.springframework.ide.eclipse.boot.test.BootProjectTestHarness.withStarters;
 
@@ -44,12 +45,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.InOrder;
-import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
-import org.omg.CORBA.UNKNOWN;
-import org.omg.PortableInterceptor.INACTIVE;
 import org.springframework.ide.eclipse.boot.dash.cloudfoundry.CloudAppDashElement;
 import org.springframework.ide.eclipse.boot.dash.cloudfoundry.CloudFoundryBootDashModel;
 import org.springframework.ide.eclipse.boot.dash.cloudfoundry.CloudFoundryRunTargetType;
@@ -740,8 +737,7 @@ public class CloudFoundryBootDashModelMockingTest {
 	@Test public void stopCancelsDeploy() throws Exception {
 		CFClientParams targetParams = CfTestTargetParams.fromEnv();
 		clientFactory.defSpace(targetParams.getOrgName(), targetParams.getSpaceName());
-		CloudFoundryBootDashModel target =  harness.createCfTarget(targetParams);
-		final CloudFoundryBootDashModel model = harness.getCfTargetModel();
+		CloudFoundryBootDashModel model =  harness.createCfTarget(targetParams);
 
 		IProject project = projects.createBootProject("to-deploy", withStarters("actuator", "web"));
 
