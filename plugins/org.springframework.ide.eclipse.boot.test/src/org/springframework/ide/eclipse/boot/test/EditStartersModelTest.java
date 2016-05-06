628cc6329e744907e0bac6d5cc8fe813e5e5cade
Kris De Volder <kdevolder@pivotal.io>
2016-05-06 09:00:59 -0700

    Boot 1.3.4 releases, update version in test code

--- plugins/org.springframework.ide.eclipse.boot.test/src/org/springframework/ide/eclipse/boot/test/EditStartersModelTest.java
+++ plugins/org.springframework.ide.eclipse.boot.test/src/org/springframework/ide/eclipse/boot/test/EditStartersModelTest.java
@@ -73,7 +73,7 @@ import org.w3c.dom.Element;
 @SuppressWarnings("restriction")
 public class EditStartersModelTest {
 
-	private static final String BOOT_1_3_X_RELEASE = "1.3.3.RELEASE";
+	private static final String BOOT_1_3_X_RELEASE = "1.3.4.RELEASE";
 	private static final String REPOSITORY = "repository";
 	private static final String REPOSITORIES = "repositories";
 	private MockInitializrService initializr = new MockInitializrService();
