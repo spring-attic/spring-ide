9b1c6125af5d268ce6e5709ce328df8d5d2a31ca
nsingh <nsingh@pivotal.io>
2016-05-24 15:36:40 -0700

    Refactored import static feature into new plug-in
    Created org.springframework.ide.eclipse.imports plug-in.
    Also added support for early startup using IStartup to load import
    static favorites at startup.

--- plugins/org.springframework.ide.eclipse.ui/src/org/springframework/ide/eclipse/ui/SpringUIPlugin.java
+++ plugins/org.springframework.ide.eclipse.ui/src/org/springframework/ide/eclipse/ui/SpringUIPlugin.java
@@ -25,8 +25,6 @@ import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
-import org.springframework.ide.eclipse.ui.imports.SpringStaticImportFavourites;
-import org.springframework.ide.eclipse.ui.imports.StaticImportCatalogue;
 import org.springsource.ide.eclipse.commons.ui.ImageDescriptorRegistry;
 
 /**
@@ -72,12 +70,6 @@ public class SpringUIPlugin extends AbstractUIPlugin {
 	 */
 	public SpringUIPlugin() {
 		plugin = this;
-		initImportFavourites();
-	}
-	
-	public static void initImportFavourites() {
-		SpringStaticImportFavourites importFavourites = new SpringStaticImportFavourites(StaticImportCatalogue.DEFAULT_CATALOGUE);
-		importFavourites.asynchLoad();
 	}
 
 	@Override
