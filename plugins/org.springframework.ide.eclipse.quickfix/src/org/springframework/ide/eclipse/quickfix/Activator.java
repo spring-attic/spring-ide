899325f2faf5c4ee83b526476571fee3ce58d929
nsingh <nsingh@pivotal.io>
2016-05-31 19:24:59 -0700

    Initial implementation for Add Imports quick fix
    JDT Quick Fix processor is disabled by this feature and replaced by an
    STS version that does the same functionality with additional support for
    reordering proposals based on additional criteria.

--- plugins/org.springframework.ide.eclipse.quickfix/src/org/springframework/ide/eclipse/quickfix/Activator.java
+++ plugins/org.springframework.ide.eclipse.quickfix/src/org/springframework/ide/eclipse/quickfix/Activator.java
@@ -1,18 +1,20 @@
 /*******************************************************************************
- *  Copyright (c) 2012 VMware, Inc.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2012, 2016 Pivotal, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- *  Contributors:
- *      VMware, Inc. - initial API and implementation
+ * Contributors:
+ *     Pivotal, Inc. - initial API and implementation
  *******************************************************************************/
-
 package org.springframework.ide.eclipse.quickfix;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+import org.springsource.ide.eclipse.commons.core.StatusHandler;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -37,10 +39,9 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
-	 * )
+	 *
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.
+	 * BundleContext )
 	 */
 	@Override
 	public void start(BundleContext context) throws Exception {
@@ -50,10 +51,9 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
-	 * )
+	 *
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.
+	 * BundleContext )
 	 */
 	@Override
 	public void stop(BundleContext context) throws Exception {
@@ -63,11 +63,24 @@ public class Activator extends AbstractUIPlugin {
 
 	/**
 	 * Returns the shared instance
-	 * 
+	 *
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
 		return plugin;
+	}
+
+	public static void log(Throwable e, String message) {
+		StatusHandler.log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, message, e));
+	}
+
+	public static void log(Throwable e) {
+		log(e, e.getMessage());
+	}
+
+	public static void logInfo(String message) {
+		StatusHandler.log(new Status(IStatus.INFO, Activator.PLUGIN_ID, message));
+
 	}
 
 }
---
+++ plugins/org.springframework.ide.eclipse.quickfix/src/org/springframework/ide/eclipse/quickfix/jdt/processors/imports/AddImportRelevanceResolverFactory.java
@@ -0,0 +1,98 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Pivotal, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Pivotal, Inc. - initial API and implementation
+ *******************************************************************************/
+package org.springframework.ide.eclipse.quickfix.jdt.processors.imports;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
+import org.eclipse.jdt.internal.ui.text.correction.proposals.AddImportCorrectionProposal;
+import org.eclipse.jdt.ui.text.java.IJavaCompletionProposal;
+
+/**
+ * Factory that creates various types of relevance resolvers for "add imports"
+ * proposals based on the type of selected AST node where "add imports" quick
+ * fix should be applied to
+ *
+ */
+public class AddImportRelevanceResolverFactory {
+
+	public List<AddImportRelevanceResolver> getResolvers(ICompilationUnit cu, List<IJavaCompletionProposal> proposals) {
+		List<AddImportRelevanceResolver> computations = new ArrayList<AddImportRelevanceResolver>();
+
+		int boost = 100;
+		computations.add(variableDeclarationRHSReturnType(cu, proposals, boost));
+
+		return computations;
+	}
+
+	/**
+	 * Computes relevance for proposals that match the return type on the RHS of
+	 * a statement where LHS has unresolved type
+	 * @param kind
+	 * @param cu
+	 * @param proposals
+	 * @param boost
+	 * @return
+	 */
+	protected AddImportRelevanceResolver variableDeclarationRHSReturnType(ICompilationUnit cu,
+			List<IJavaCompletionProposal> proposals, int boost) {
+
+		return new AddImportRelevanceResolver(cu, proposals, boost) {
+
+			protected VariableDeclarationStatement getVariableDeclarationStatement(ASTNode statementNode) {
+				// Go two levels up:
+				ASTNode parent = statementNode.getParent();
+				if (parent instanceof VariableDeclarationStatement) {
+					return (VariableDeclarationStatement) parent;
+				}
+				else if (parent.getParent() instanceof VariableDeclarationStatement) {
+					return (VariableDeclarationStatement) parent.getParent();
+				}
+				return null;
+			}
+
+			@Override
+			protected AddImportCorrectionProposal findCorrectionProposal(int kind, ASTNode statementNode)
+					throws Exception {
+				VariableDeclarationStatement statement = getVariableDeclarationStatement(statementNode);
+				if (statement != null) {
+
+					List<?> frags = statement.fragments();
+					for (Object content : frags) {
+						if (content instanceof VariableDeclarationFragment) {
+							VariableDeclarationFragment frag = (VariableDeclarationFragment) content;
+							Expression exp = frag.getInitializer();
+							ITypeBinding binding = exp.resolveTypeBinding();
+							String qualifiedName = binding.getQualifiedName();
+
+							for (IJavaCompletionProposal prop : proposals) {
+								if (prop instanceof AddImportCorrectionProposal) {
+									AddImportCorrectionProposal corrProposal = (AddImportCorrectionProposal) prop;
+									String propQuali = corrProposal.getQualifiedTypeName();
+									if (qualifiedName.contains(propQuali)) {
+										return corrProposal;
+									}
+								}
+							}
+						}
+					}
+				}
+				return null;
+			}
+		};
+	}
+}
