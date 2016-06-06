5571fbb73751dc609f2d01be8feb11961edb65eb
nsingh <nsingh@pivotal.io>
2016-06-06 14:27:47 -0700

    Refactored YValueHint into interface

--- plugins/org.springframework.ide.eclipse.cloudfoundry.manifest.editor/src/org/springframework/ide/eclipse/cloudfoundry/manifest/editor/YValueHint.java
+++ plugins/org.springframework.ide.eclipse.cloudfoundry.manifest.editor/src/org/springframework/ide/eclipse/cloudfoundry/manifest/editor/YValueHint.java
@@ -10,57 +10,10 @@
  *******************************************************************************/
 package org.springframework.ide.eclipse.cloudfoundry.manifest.editor;
 
-public class YValueHint {
+public interface YValueHint {
 
-	private final String value;
-	private String label;
+	String getValue();
 
-	public YValueHint(String value, String label) {
-		this.value = value;
-		this.label = label;
-	}
+	String getLabel();
 
-	public String getValue() {
-		return value;
-	}
-
-	public String getLabel() {
-		return label;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((label == null) ? 0 : label.hashCode());
-		result = prime * result + ((value == null) ? 0 : value.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		YValueHint other = (YValueHint) obj;
-		if (label == null) {
-			if (other.label != null)
-				return false;
-		} else if (!label.equals(other.label))
-			return false;
-		if (value == null) {
-			if (other.value != null)
-				return false;
-		} else if (!value.equals(other.value))
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "YValueHint [value=" + value + ", label=" + label + "]";
-	}
 }
