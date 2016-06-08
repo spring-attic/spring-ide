7529f85fb904114d93bcfe5c4c1342af6ddbcb31
BoykoAlex <aboyko@pivotal.io>
2016-06-08 13:23:51 -0400

    [PT-#115286901] Activate view instead of making it visible

--- plugins/org.springframework.ide.eclipse.boot.dash/src/org/springframework/ide/eclipse/boot/dash/views/ShowViewAction.java
+++ plugins/org.springframework.ide.eclipse.boot.dash/src/org/springframework/ide/eclipse/boot/dash/views/ShowViewAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Pivotal, Inc.
+ * Copyright (c) 2015, 2016 Pivotal, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -60,7 +60,7 @@ public class ShowViewAction extends Action {
 			IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
 			if (window != null && window.getActivePage() != null) {
 				try {
-					window.getActivePage().showView(viewId, null, IWorkbenchPage.VIEW_VISIBLE);
+					window.getActivePage().showView(viewId, null, IWorkbenchPage.VIEW_ACTIVATE);
 				} catch (PartInitException e) {
 					Log.log(e);
 				}
