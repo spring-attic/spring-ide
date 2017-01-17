e087ae25c7b4125a599f41aae3f2ecc6310289f0
Kris De Volder <kdevolder@pivotal.io>
2017-01-17 10:06:14 -0800

    Fix buildship suport to workt with BS 2.0

--- plugins/org.springframework.ide.eclipse.buildship/src/org/springframework/ide/eclipse/buildship/BuildshipImportStrategy.java
+++ plugins/org.springframework.ide.eclipse.buildship/src/org/springframework/ide/eclipse/buildship/BuildshipImportStrategy.java
@@ -13,20 +13,20 @@ package org.springframework.ide.eclipse.buildship;
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
+import java.util.concurrent.CompletableFuture;
 
-import org.eclipse.buildship.core.projectimport.ProjectImportConfiguration;
-import org.eclipse.buildship.core.util.gradle.GradleDistributionWrapper;
-import org.eclipse.buildship.core.util.gradle.GradleDistributionWrapper.DistributionType;
-import org.eclipse.buildship.core.util.progress.AsyncHandler;
-import org.eclipse.buildship.core.workspace.SynchronizeGradleProjectJob;
+import org.eclipse.buildship.core.CorePlugin;
+import org.eclipse.buildship.core.util.configuration.FixedRequestAttributesBuilder;
+import org.eclipse.buildship.core.workspace.NewProjectHandler;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Assert;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.SubProgressMonitor;
-import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.springframework.ide.eclipse.boot.wizard.content.BuildType;
 import org.springframework.ide.eclipse.boot.wizard.importing.ImportConfiguration;
@@ -34,8 +34,11 @@ import org.springframework.ide.eclipse.boot.wizard.importing.ImportStrategy;
 import org.springframework.ide.eclipse.boot.wizard.importing.ImportStrategyFactory;
 import org.springsource.ide.eclipse.commons.core.SpringCoreUtils;
 import org.springsource.ide.eclipse.commons.core.util.NatureUtils;
+import org.springsource.ide.eclipse.commons.frameworks.core.FrameworkCoreActivator;
+import org.springsource.ide.eclipse.commons.livexp.util.ExceptionUtil;
 
-import com.google.common.collect.ImmutableList;
+import com.gradleware.tooling.toolingmodel.OmniEclipseProject;
+import com.gradleware.tooling.toolingmodel.repository.FixedRequestAttributes;
 
 /**
  * Importer strategy implementation for importing CodeSets into the workspace and set them
@@ -43,11 +46,25 @@ import com.google.common.collect.ImmutableList;
  *
  * @author Kris De Volder
  */
+@SuppressWarnings("restriction")
 public class BuildshipImportStrategy extends ImportStrategy {
 
-	//TODO: the way progress is reported with the sub-job / join is not very nice (a double progress popup appears)
-	// Perhaps this so post has the answer to doing it a better way:
-	//   http://stackoverflow.com/questions/14530200/eclipse-jobs-api-how-to-track-progress-for-job-scheduled-by-another-job
+	protected static final NewProjectHandler IMPORT_AND_ADD_SPRING_NATURE = new NewProjectHandler() {
+
+		@Override
+		public boolean shouldImport(OmniEclipseProject projectModel) {
+			return true;
+		}
+
+		@Override
+		public void afterImport(IProject p, OmniEclipseProject projectModel) {
+			try {
+				NatureUtils.ensure(p, new NullProgressMonitor(), SpringCoreUtils.NATURE_ID);
+			} catch (CoreException e) {
+				FrameworkCoreActivator.log(e);
+			}
+		}
+	};
 
 	public BuildshipImportStrategy(BuildType buildType, String name, String notInstalledMessage) {
 		super(buildType, name, notInstalledMessage);
@@ -103,29 +120,29 @@ public class BuildshipImportStrategy extends ImportStrategy {
 					File loc = new File(conf.getLocation());
 					conf.getCodeSet().createAt(loc);
 
-					ProjectImportConfiguration conf = new ProjectImportConfiguration();
-					conf.setProjectDir(loc);
-					conf.setGradleDistribution(createGradleDistribution());
-					conf.setApplyWorkingSets(false);
-					SynchronizeGradleProjectJob job = new SynchronizeGradleProjectJob(
-							conf.toFixedAttributes(), ImmutableList.<String>of(),
-							AsyncHandler.NO_OP);
-					job.schedule();
-
-					// This doesn't work in e44 (api not available until e45):
-					job.join(0, new SubProgressMonitor(mon, 9));
-					// The below works, but makes more assumptions on the internal workings of SynchronizeGradleProjectJob
-					// Namely: how it implements Job.belongsTo to make itself belong to program family equal to its class name.
-					//Job.getJobManager().join(SynchronizeGradleProjectJob.class.getName(), new SubProgressMonitor(mon, 9));
-
-					IProject p = getProject(loc);
-					if (p!=null) {
-						NatureUtils.ensure(p, new SubProgressMonitor(mon, 1), SpringCoreUtils.NATURE_ID);
-					}
-				} catch (InterruptedException|InvocationTargetException e) {
-					throw e;
+					CompletableFuture<Void> done = new CompletableFuture<>();
+					FixedRequestAttributes attributes = FixedRequestAttributesBuilder.fromWorkspaceSettings(loc)
+						.build();
+					CorePlugin.gradleWorkspaceManager().getGradleBuild(attributes).synchronize(IMPORT_AND_ADD_SPRING_NATURE,
+							(monitor, cancelation) -> {
+								if (monitor.isCanceled() || cancelation.isCancellationRequested()) {
+									done.cancel(true);
+								} else {
+									done.complete(null);
+								}
+							}
+					);
+					done.get();
 				} catch (Exception e) {
-					throw new InvocationTargetException(e);
+					if (!ExceptionUtil.isCancelation(e)) {
+						if (e instanceof InterruptedException) {
+							throw (InterruptedException)e;
+						}
+						if (e instanceof InvocationTargetException) {
+							throw (InvocationTargetException)e;
+						}
+						throw new InvocationTargetException(e);
+					}
 				}
 				finally {
 					mon.done();
@@ -133,12 +150,5 @@ public class BuildshipImportStrategy extends ImportStrategy {
 			}
 		};
 	}
-
-    private GradleDistributionWrapper createGradleDistribution() {
-        DistributionType distributionType = DistributionType.WRAPPER;
-        String distributionConfiguration = null;
-        return GradleDistributionWrapper.from(distributionType, distributionConfiguration);
-    }
-
 
 }
