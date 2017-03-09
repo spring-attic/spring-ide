b175307ca607f5bb4e994236584bdb95297c93f7
Kris De Volder <kdevolder@pivotal.io>
2017-03-08 17:36:22 -0800

    Wire the Validator of DynamicSection to its content's validator.

--- plugins/org.springframework.ide.eclipse.boot.wizard/src/org/springframework/ide/eclipse/boot/livexp/ui/DynamicSection.java
+++ plugins/org.springframework.ide.eclipse.boot.wizard/src/org/springframework/ide/eclipse/boot/livexp/ui/DynamicSection.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Pivotal, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Pivotal, Inc. - initial API and implementation
+ *******************************************************************************/
 package org.springframework.ide.eclipse.boot.livexp.ui;
 
 import org.eclipse.jface.layout.GridDataFactory;
@@ -7,6 +17,7 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Layout;
 import org.springsource.ide.eclipse.commons.livexp.core.LiveExpression;
+import org.springsource.ide.eclipse.commons.livexp.core.ValidationResult;
 import org.springsource.ide.eclipse.commons.livexp.ui.IPageSection;
 import org.springsource.ide.eclipse.commons.livexp.ui.IPageWithSections;
 import org.springsource.ide.eclipse.commons.livexp.ui.ReflowableSection;
@@ -15,6 +26,8 @@ public class DynamicSection extends ReflowableSection {
 
 	private LiveExpression<IPageSection> content;
 	private Composite composite;
+
+	private final DelegatingLiveExp<ValidationResult> validator = new DelegatingLiveExp<>();
 
 	public DynamicSection(IPageWithSections owner, LiveExpression<IPageSection> content) {
 		super(owner);
@@ -32,12 +45,19 @@ public class DynamicSection extends ReflowableSection {
 
 	private void updateContent(IPageSection newContents) {
 		if (composite!=null && !composite.isDisposed()) {
+			validator.setDelegate(newContents.getValidator());
 			for (Control oldWidget : composite.getChildren()) {
 				oldWidget.dispose();
 			}
+			newContents.createContents(composite);
+			reflow(owner, composite);
+		} else {
+			validator.setDelegate(null);
 		}
-		newContents.createContents(composite);
-		reflow(owner, composite);
 	}
 
+	@Override
+	public LiveExpression<ValidationResult> getValidator() {
+		return validator;
+	}
 }
