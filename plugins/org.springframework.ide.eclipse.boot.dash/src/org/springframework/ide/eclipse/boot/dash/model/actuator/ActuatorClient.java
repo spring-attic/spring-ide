f8141e7e9848c9353b05e9ce66c3d571d68bf730
Martin Lippert <mlippert@pivotal.io>
2018-01-24 14:03:31 +0100

    fix issue with parsing request mapping data that is coming from the rest actuator client instead of the JMX one

--- plugins/org.springframework.ide.eclipse.boot.dash/src/org/springframework/ide/eclipse/boot/dash/model/actuator/ActuatorClient.java
+++ plugins/org.springframework.ide.eclipse.boot.dash/src/org/springframework/ide/eclipse/boot/dash/model/actuator/ActuatorClient.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015-2017 Pivotal, Inc.
+ * Copyright (c) 2015, 2018 Pivotal, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -47,7 +47,7 @@ public abstract class ActuatorClient {
 	private List<RequestMapping> parseRequestMappings(String json, String version) throws JSONException {
  		JSONObject obj = new JSONObject(json);
  		RequestMappingsParser parser;
-		if (version.equals("2")) {
+		if ("2".equals(version)) {
 			// Boot 2.x
 			parser = new Boot2RequestMappingsParser();
 		} else {
