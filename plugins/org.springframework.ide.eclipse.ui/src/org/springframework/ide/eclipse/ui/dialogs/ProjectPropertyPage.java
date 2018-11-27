84a5bbb3e3dbc63d92aa1a90ef7933d533c6d0c5
BoykoAlex <aboyko@pivotal.io>
2018-11-27 16:05:29 -0500

    PT: #152576044: Add project properties pages under Spring category from commons

--- plugins/org.springframework.ide.eclipse.ui/src/org/springframework/ide/eclipse/ui/dialogs/ProjectPropertyPage.java
+++ plugins/org.springframework.ide.eclipse.ui/src/org/springframework/ide/eclipse/ui/dialogs/ProjectPropertyPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2015 Spring IDE Developers
+ * Copyright (c) 2007, 2018 Spring IDE Developers
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -41,7 +41,7 @@ public class ProjectPropertyPage extends ProjectAndPreferencePage {
 
 	public static final String PREF_ID = "org.springframework.ide.eclipse.ui.preferencePage"; //$NON-NLS-1$
 
-	public static final String PROP_ID = "org.springframework.ide.eclipse.ui.projectPropertyPage"; //$NON-NLS-1$
+	public static final String PROP_ID = "org.springframework.ide.eclipse.ui.validationPropertyPage"; //$NON-NLS-1$
 
 	private ProjectBuilderPropertyTab builderTab = null;
 
