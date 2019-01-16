5b365ee1c6e1a0813ca8761f65c4b9d2046a5dbc
nsingh@pivotal.io <nsingh@pivotal.io>
2019-01-16 02:12:24 -0800

    Minor type change

--- plugins/org.springframework.ide.eclipse.beans.ui.live/src/org/springframework/ide/eclipse/environment/ui/live/model/LiveEnvJsonParser2x.java
+++ plugins/org.springframework.ide.eclipse.beans.ui.live/src/org/springframework/ide/eclipse/environment/ui/live/model/LiveEnvJsonParser2x.java
@@ -93,7 +93,7 @@ public class LiveEnvJsonParser2x implements JsonParser<LiveEnvModel> {
 				}
 			}
 		}
-		ImmutableList<PropertySource> sources = allSources.build();
+		List<PropertySource> sources = allSources.build();
 		return new PropertySources(sources);
 	}
 
