386ad99ee02ba75bbee08358fd3c26c520363410
nsingh@pivotal.io <nsingh@pivotal.io>
2019-01-18 01:53:44 -0800

    Adopt common searchable tree in Beans section

--- plugins/org.springframework.ide.eclipse.boot.dash/src/org/springframework/ide/eclipse/boot/dash/views/properties/BeansPropertiesSection.java
+++ plugins/org.springframework.ide.eclipse.boot.dash/src/org/springframework/ide/eclipse/boot/dash/views/properties/BeansPropertiesSection.java
@@ -10,30 +10,18 @@
  *******************************************************************************/
 package org.springframework.ide.eclipse.boot.dash.views.properties;
 
-import org.eclipse.jface.layout.GridDataFactory;
-import org.eclipse.jface.layout.GridLayoutFactory;
+import java.util.function.Supplier;
+
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.jface.viewers.TreeViewer;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.StackLayout;
-import org.eclipse.swt.events.TreeEvent;
-import org.eclipse.swt.events.TreeListener;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.views.properties.tabbed.ITabbedPropertyConstants;
 import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 import org.springframework.ide.eclipse.beans.ui.live.tree.ContextGroupedBeansContentProvider;
 import org.springframework.ide.eclipse.beans.ui.live.tree.LiveBeansTreeLabelProvider;
 import org.springframework.ide.eclipse.boot.dash.model.BootDashElement;
-import org.springsource.ide.eclipse.commons.livexp.core.FilterBoxModel;
-import org.springsource.ide.eclipse.commons.livexp.ui.util.SwtConnect;
 import org.springsource.ide.eclipse.commons.livexp.ui.util.TreeElementWrappingContentProvider;
-import org.springsource.ide.eclipse.commons.livexp.util.Filter;
-import org.springsource.ide.eclipse.commons.livexp.util.Filters;
 
 /**
  * Live beans property section
@@ -43,104 +31,49 @@ import org.springsource.ide.eclipse.commons.livexp.util.Filters;
  */
 public class BeansPropertiesSection extends AbstractBdePropertiesSection {
 
-	private TreeViewer treeViewer;
-	private Label missingContentsLabel;
 	private TabbedPropertySheetPage page;
-	private StackLayout layout;
-	private Text searchBox;
-	private Composite treeViewerComposite;
+	private SearchableTreePage treePage;
+
 
 	@Override
 	public void createControls(Composite parent, TabbedPropertySheetPage aTabbedPropertySheetPage) {
 		super.createControls(parent, aTabbedPropertySheetPage);
 		page = aTabbedPropertySheetPage;
 
-		Composite composite = getWidgetFactory().createComposite(parent, SWT.NONE);
-
-		composite.setLayout(layout = new SectionStackLayout());
-
-		layout.marginWidth = ITabbedPropertyConstants.HSPACE + 2;
-		layout.marginHeight = ITabbedPropertyConstants.VSPACE + 4;
-
-		missingContentsLabel = getWidgetFactory().createLabel(composite, "", SWT.WRAP); //$NON-NLS-1$
-
-		treeViewerComposite = new Composite(composite, SWT.NONE);
-		treeViewerComposite.setLayout(GridLayoutFactory.fillDefaults().margins(0, 0).spacing(1, 1).numColumns(1).create());
-
-		searchBox = getWidgetFactory().createText(treeViewerComposite, "", SWT.SINGLE | SWT.BORDER | SWT.SEARCH | SWT.ICON_CANCEL);
-		searchBox.setMessage("Enter search string");
-		GridDataFactory.fillDefaults().grab(true, false).applyTo(searchBox);
-		FilterBoxModel<String> searchBoxModel = new FilterBoxModel<String>() {
-			@Override
-			protected Filter<String> createFilterForInput(String pattern) {
-				return Filters.caseInsensitiveSubstring(pattern);
-			}
-		};
-		SwtConnect.connect(searchBox, searchBoxModel.getText());
-		searchBox.addDisposeListener(de -> searchBoxModel.close());
-
-		treeViewer = new TreeViewer(treeViewerComposite /*, SWT.NO_SCROLL*/);
-
 		ITreeContentProvider treeContent = new TreeElementWrappingContentProvider(new BeansContentProvider(ContextGroupedBeansContentProvider.INSTANCE));
 		LabelProvider labelProvider = LiveBeansTreeLabelProvider.INSTANCE;
 
-		treeViewer.setContentProvider(treeContent);
-		treeViewer.setLabelProvider(labelProvider);
-		treeViewer.setAutoExpandLevel(2);
-
-		treeViewer.getTree().addTreeListener (new TreeListener () {
-			@Override
-			public void treeExpanded (TreeEvent e) {
-				SectionStackLayout.reflow(page);
-			}
-			@Override
-			public void treeCollapsed (TreeEvent e) {
-				SectionStackLayout.reflow(page);
-			}
-		});
-
-		SwtConnect.connectTextBasedFilter(treeViewer, searchBoxModel.getFilter(), labelProvider, treeContent);
-		GridDataFactory.fillDefaults().grab(true, true).applyTo(treeViewer.getTree());
-
-		refreshControlsVisibility();
+		treePage = new SearchableTreePage(getWidgetFactory(), getNoContentMessage());
+		treePage.createControls(parent, page, treeContent, labelProvider);
 	}
 
+	@Override
 	public void setInput(IWorkbenchPart part, ISelection selection) {
 		super.setInput(part, selection);
 		// BootDashElement should be input rather than the LiveBeansModel. Due to
 		// polling the model often to show changes in the model it's best to refresh the
 		// tree viewer rather then set the whole input that would remove the selection
 		// and collapse expanded nodes
-		treeViewer.setInput(getBootDashElement());
+		treePage.getTreeViewer().setInput(getBootDashElement());
 	}
 
+	@Override
 	public void refresh() {
-		refreshControlsVisibility();
-		BootDashElement bde = getBootDashElement();
-		if (bde == null) {
-			missingContentsLabel.setText("Select single element in Boot Dashboard to see live Beans");
-		} else if (bde.getLiveBeans() == null) {
-			missingContentsLabel.setText("'" + bde.getName() + "' must be running with JMX enabled; and actuator 'beans' endpoint must be enabled to obtain beans.");
-		} else {
-			missingContentsLabel.setText("");
-		}
-		// No tree widgets means that BootDashElement probably wasn't "running" or had no beans
-		boolean firstTimeTreePopulated = treeViewer.getTree().getItems().length == 0;
-		treeViewer.refresh();
-		if (firstTimeTreePopulated) {
-			// If tree is populated for the first time then auto expand to level 2 manually because new input is not set in this case
-			treeViewer.expandToLevel(2);
-		}
-		SectionStackLayout.reflow(page);
+		treePage.refresh();
 	}
 
-	private void refreshControlsVisibility() {
-		BootDashElement bde = getBootDashElement();
-		if (bde == null || bde.getLiveRequestMappings() == null) {
-			layout.topControl = missingContentsLabel;
-		} else {
-			layout.topControl = treeViewerComposite;
-		}
+	private Supplier<String> getNoContentMessage() {
+		return () -> {
+			BootDashElement bde = getBootDashElement();
+			if (bde == null) {
+				return "Select single element in Boot Dashboard to see live Beans";
+			} else if (bde.getLiveBeans() == null) {
+				return "'" + bde.getName()
+						+ "' must be running with JMX enabled; and actuator 'beans' endpoint must be enabled to obtain beans.";
+			} else {
+				return null;
+			}
+		};
 	}
 
 	private static class BeansContentProvider implements ITreeContentProvider {
@@ -174,7 +107,5 @@ public class BeansPropertiesSection extends AbstractBdePropertiesSection {
 		public boolean hasChildren(Object element) {
 			return delegateContentProvider.hasChildren(element);
 		}
-
 	}
-
 }
