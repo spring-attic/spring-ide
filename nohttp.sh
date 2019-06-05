fcef9e5092d02df95f01d567e7dd89c63afb5f21
Kris De Volder <kdevolder@pivotal.io>
2019-06-05 13:54:14 -0700

    Tweak nohttp script to run on bamboo

--- nohttp.sh
+++ nohttp.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 VERSION=0.0.2.RELEASE
+if [ ! -z ${bamboo_JDK8_HOME} ]; then
+    # put java 8 on the path when running on bamboo
+    export PATH=${bamboo_JDK8_HOME}/bin/${PATH}
+fi
 if [ ! -f nohttp-cli-${VERSION}.jar ]; then
     curl -O https://repo.maven.apache.org/maven2/io/spring/nohttp/nohttp-cli/${VERSION}/nohttp-cli-${VERSION}.jar
 fi
