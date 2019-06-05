d947e8fc1a3f5669ad8a4fb0ef85a2a782f2938b
Kris De Volder <kdevolder@pivotal.io>
2019-06-05 13:58:21 -0700

    Another attempt to fix nohttp on bamboo

--- nohttp.sh
+++ nohttp.sh
@@ -3,7 +3,8 @@ set -e
 VERSION=0.0.2.RELEASE
 if [ ! -z ${bamboo_JDK8_HOME} ]; then
     # put java 8 on the path when running on bamboo
-    export PATH=${bamboo_JDK8_HOME}/bin/${PATH}
+    echo "Running on bamboo. Put JDK on path: ${bamboo_JDK8_HOME}/bin"
+    export PATH=${bamboo_JDK8_HOME}/bin/:${PATH}
 fi
 if [ ! -f nohttp-cli-${VERSION}.jar ]; then
     curl -O https://repo.maven.apache.org/maven2/io/spring/nohttp/nohttp-cli/${VERSION}/nohttp-cli-${VERSION}.jar
