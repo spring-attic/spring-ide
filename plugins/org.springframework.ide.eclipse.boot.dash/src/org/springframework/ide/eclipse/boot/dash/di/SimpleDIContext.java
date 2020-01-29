b907f068af04b9b42aa6146bb27fd3b83849586b
Kris De Volder <kdevolder@pivotal.io>
2020-01-29 10:40:43 -0800

    Cleanup import

--- plugins/org.springframework.ide.eclipse.boot.dash/src/org/springframework/ide/eclipse/boot/dash/di/SimpleDIContext.java
+++ plugins/org.springframework.ide.eclipse.boot.dash/src/org/springframework/ide/eclipse/boot/dash/di/SimpleDIContext.java
@@ -19,7 +19,6 @@ import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import org.eclipse.core.runtime.Assert;
-import org.springframework.ide.eclipse.boot.dash.model.BootDashViewModel;
 import org.springsource.ide.eclipse.commons.livexp.util.ExceptionUtil;
 
 import com.google.common.cache.Cache;
