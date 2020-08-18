4e56e8db8e8467b74c8bfc2c494bc69e475fc782
Nieraj Singh <nierajsingh@vmware.com>
2020-08-18 15:10:15 -0700

    PT 173460477 - Switch to http for default Windows docker URL

--- plugins/org.springframework.ide.eclipse.boot.dash.docker/src/org/springframework/ide/eclipse/boot/dash/docker/ui/SelectDockerDaemonDialog.java
+++ plugins/org.springframework.ide.eclipse.boot.dash.docker/src/org/springframework/ide/eclipse/boot/dash/docker/ui/SelectDockerDaemonDialog.java
@@ -31,7 +31,7 @@ public class SelectDockerDaemonDialog extends DialogWithSections {
 
 	public static class Model implements OkButtonHandler {
 		private static final String DEFAULT_UNIX_DOCKER_URL = "unix:///var/run/docker.sock";
-		private static final String DEFAUL_WINDOWS_DOCKER_URL = "https://localhost:2375";
+		private static final String DEFAUL_WINDOWS_DOCKER_URL = "http://localhost:2375";
 		
 		public final LiveVariable<Boolean> useLocalDaemon = new LiveVariable<>(true);
 		public final LiveExpression<Boolean> daemonUrlEnabled = useLocalDaemon.apply(local -> !local);
