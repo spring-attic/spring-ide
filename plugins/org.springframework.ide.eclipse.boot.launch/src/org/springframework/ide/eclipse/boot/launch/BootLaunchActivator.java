a3934421e3c758976d8361e134623ec7d5727407
Kris De Volder <kdevolder@pivotal.io>
2020-09-16 12:13:47 -0700

    Disable cglib breakpoint warning

--- plugins/org.springframework.ide.eclipse.boot.launch/src/org/springframework/ide/eclipse/boot/launch/BootLaunchActivator.java
+++ plugins/org.springframework.ide.eclipse.boot.launch/src/org/springframework/ide/eclipse/boot/launch/BootLaunchActivator.java
@@ -11,10 +11,15 @@
 package org.springframework.ide.eclipse.boot.launch;
 
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.service.prefs.BackingStoreException;
 import org.springframework.ide.eclipse.boot.launch.util.BootLaunchConfDeleter;
 
 /**
@@ -34,6 +39,29 @@ public class BootLaunchActivator extends AbstractUIPlugin {
 		super.start(context);
 		workspaceListener = new BootLaunchConfDeleter(ResourcesPlugin.getWorkspace(), DebugPlugin.getDefault().getLaunchManager());
 		instance = this;
+
+		IPreferenceStore myStore = instance.getPreferenceStore();
+		if (!myStore.getBoolean("cglib.breakpoint.warning.disabled")) {
+			setPreference("org.eclipse.jdt.debug.ui", "org.eclipse.jdt.debug.ui.prompt_unable_to_install_breakpoint", false);
+			myStore.setValue("cglib.breakpoint.warning.disabled", true);
+		}
+	}
+
+	private void setPreference(String plugin, String key, boolean value) {
+		try {
+			IEclipsePreferences store = DefaultScope.INSTANCE.getNode(plugin);
+			store.putBoolean(key, value);
+			store.flush();
+		} catch (BackingStoreException e) {
+			//ignore
+		}
+		try {
+			IEclipsePreferences store = InstanceScope.INSTANCE.getNode(plugin);
+			store.putBoolean(key, value);
+			store.flush();
+		} catch (BackingStoreException e) {
+			//ignore
+		}
 	}
 
 	@Override
